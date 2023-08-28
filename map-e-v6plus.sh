a70522dd7ccad0a58fd0d47c33e6fb439075fd7e
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-08-28 23:13:57 +0900

    Update map-e-v6plus.sh

--- map-e-v6plus.sh
+++ map-e-v6plus.sh
@@ -865,7 +865,7 @@ uci set dhcp.wan6.dhcpv6='relay'
 uci set dhcp.wan6.ndp='relay'
 
 # WAN6
-uci set network.wan6.ip6prefix=${CE_CE}::/56
+uci set network.wan6.ip6prefix=${CE}::/56
 
 # WANMAP
 WANMAP='wanmap'
