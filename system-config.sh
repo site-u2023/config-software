3fbe0fed78551cf4e77a3531b6bd4d30577be688
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-08-28 16:29:26 +0900

    Update system-config.sh

--- system-config.sh
+++ system-config.sh
@@ -8,7 +8,7 @@ do
   read -p " デバイスの名称: " input_str_SYSTEM_HOSTNAME
   read -p " 本当に宜しいですか? [y/n or e]: " num
   case "${num}" in
-    "y" ) sed -i -e "s/HOSTNAME='openwrt'/HOSTNAME=${input_str_SYSTEM_HOSTNAME}/g" /etc/device-config.sh; _func_ROOT_PASSWD ;;
+    "y" ) sed -i -e "s/HOSTNAME='openwrt'/HOSTNAME=${input_str_SYSTEM_HOSTNAME}/g" /etc/device.sh; _func_ROOT_PASSWD ;;
     "n" ) _func_HOSTNAME ;;
     "e" ) exit ;;
   esac
@@ -21,7 +21,7 @@ do
   read -p " ${input_str_SYSTEM_HOSTNAME}のパスワード: " input_str_ROOT_PASSWD
   read -p " 本当に宜しいですか? [y/n or e]: " num
   case "${num}" in
-    "y" ) sed -i -e "s/ROOT_PASSWD/${input_str_ROOT_PASSWD}/g" /etc/device-config.sh; _func_WIFI_SSID_A ;;
+    "y" ) sed -i -e "s/ROOT_PASSWD/${input_str_ROOT_PASSWD}/g" /etc/device.sh; _func_WIFI_SSID_A ;;
     "n" ) _func_ROOT_PASSWD ;;
     "e" ) exit ;;
   esac
@@ -34,7 +34,7 @@ do
   read -p " Wi-FiのSSID A: " input_str_WIFI_SSID_A
   read -p " 本当に宜しいですか? [y/n or e]: " num
   case "${num}" in
-    "y" ) sed -i -e "s/WIFI_SSID_A='SSID_A'/WIFI_SSID_A=${input_str_WIFI_SSID_A}/g" /etc/device-config.sh; _func_WIFI_PASSWD_A ;;
+    "y" ) sed -i -e "s/WIFI_SSID_A='SSID_A'/WIFI_SSID_A=${input_str_WIFI_SSID_A}/g" /etc/device.sh; _func_WIFI_PASSWD_A ;;
     "n" ) _func_WIFI_SSID_A ;;
     "e" ) exit ;;
   esac
@@ -47,7 +47,7 @@ do
   read -p " Wi-Fi Aのパスワード: " input_str_WIFI_PASSWD_A
   read -p " 本当に宜しいですか? [y/n or e]: " num
   case "${num}" in
-    "y" ) sed -i -e "s/wifi_PASSWORD_A='password'/wifi_PASSWORD_A=${input_str_WIFI_PASSWD_A}/g" /etc/device-config.sh; _func_WIFI_SSID_B ;;
+    "y" ) sed -i -e "s/wifi_PASSWORD_A='password'/wifi_PASSWORD_A=${input_str_WIFI_PASSWD_A}/g" /etc/device.sh; _func_WIFI_SSID_B ;;
     "n" ) _func_WIFI_PASSWD_A ;;
     "e" ) exit ;;
   esac
@@ -60,7 +60,7 @@ do
   read -p " Wi-FiのSSID B: " input_str_WIFI_SSID_B
   read -p " 本当に宜しいですか? [y/n or e]: " num
   case "${num}" in
-    "y" ) sed -i -e "s/WIFI_SSID_B='SSID_B'/WIFI_SSID_B=${input_str_WIFI_SSID_B}/g" /etc/device-config.sh; _func_WIFI_PASSWD_B ;;
+    "y" ) sed -i -e "s/WIFI_SSID_B='SSID_B'/WIFI_SSID_B=${input_str_WIFI_SSID_B}/g" /etc/device.sh; _func_WIFI_PASSWD_B ;;
     "n" ) _func_WIFI_SSID_B ;;
     "e" ) exit ;;
   esac
@@ -73,7 +73,7 @@ do
   read -p " Wi-Fi Bのパスワード: " input_str_WIFI_PASSWD_B
   read -p " 本当に宜しいですか? [y/n or e]: " num
   case "${num}" in
-    "y" ) sed -i -e "s/wifi_PASSWORD_B='password'/wifi_PASSWORD_B=${input_str_WIFI_PASSWD_B}/g" /etc/device-config.sh;
+    "y" ) sed -i -e "s/wifi_PASSWORD_B='password'/wifi_PASSWORD_B=${input_str_WIFI_PASSWD_B}/g" /etc/device.sh;
           sh /etc/device-config.sh;
           read -p " 何かキーを押すと${input_str_SYSTEM_HOSTNAME}が再起動します";
           reboot ;;
@@ -87,7 +87,8 @@ do
   echo -e " \033[1;37mシステム初期設定を開始します\033[0;39m"
   read -p " 開始します [y/n]:" num
   case "${num}" in
-    "y" ) _func_HOSTNAME ;;
+    "y" ) wget --no-check-certificate -O /etc/device-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/device.sh;
+          _func_HOSTNAME ;;
     "n" ) exit ;;
   esac
 done
