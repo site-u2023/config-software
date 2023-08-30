096a0278c6e023ec98abafc96691c2a7421e0a7e
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-08-30 23:30:34 +0900

    Update openwrt-config.sh

--- openwrt-config.sh
+++ openwrt-config.sh
@@ -65,10 +65,9 @@ if [ "${OPENWRT_RELEAS:0:2}" = "23" ] || [ "${OPENWRT_RELEAS:0:2}" = "21" ] || [
 fi
 }
 AVAILABLE_MEMORY=`free | fgrep 'Mem:' | awk '{ print $4 }'`
-AVAILABLE_MEMORY2=`expr ${AVAILABLE_MEMORY} / 1024`
-AVAILABLE_FLASH=`df -Th | fgrep 'overlayfs:/overlay' | awk '{ print $5 }'`
-  echo -e " \033[1;37m利用可能フラッシュサイズ: ${AVAILABLE_FLASH}\033[0;39m"
-  echo -e " \033[1;37m利用可能メモリーサイズ: ${AVAILABLE_MEMORY2}M\033[0;39m"
+AVAILABLE_FLASH=`df | fgrep 'overlayfs:/overlay' | awk '{ print $4 }'`
+  echo -e " \033[1;37m利用可能フラッシュサイズ: ${AVAILABLE_FLASH}KB\033[0;39m"
+  echo -e " \033[1;37m利用可能メモリーサイズ: ${AVAILABLE_MEMORY}KB\033[0;39m"
   echo -e " \033[1;37m推奨設定順序: システム > インターネット > パッケージ\033[0;39m" 
   echo -e " \033[1;37mopenwrt-config ------------------------------------------------\033[0;39m"
   echo -e " \033[1;34m[s]\033[0;39m": システム初期設定（USB自動判別）
@@ -77,7 +76,7 @@ AVAILABLE_FLASH=`df -Th | fgrep 'overlayfs:/overlay' | awk '{ print $5 }'`
   echo -e " \033[1;31m[a]\033[0;39m": アクセスポイント設定
   echo -e " \033[1;37m[r]\033[0;39m": スクリプトリムーブ後終了
   echo -e " \033[7;40m[q]\033[0;39m": 終了
-  echo -e " \033[1;37m------------------------------------- August 27, 2023 Ver.2.8.1\033[0;39m"
+  echo -e " \033[1;37m------------------------------------- August 27, 2023 Ver.2.8.2\033[0;39m"
   read -p " キーを選択してください [s/i/p/a or r/q]: " num
   case "${num}" in
     "s" ) _func_SYSTEM ;;
