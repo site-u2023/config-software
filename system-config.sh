53bfd367da5751acb5c4117d894cbf19f0d68aa1
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-08-30 12:47:50 +0900

    Update system-config.sh

--- system-config.sh
+++ system-config.sh
@@ -143,7 +143,9 @@ function _func_DEVICE_SET
   sed -i -e "s/WIFI_SSID_A='SSID_A'/WIFI_SSID_A=${input_str_WIFI_SSID_A}/g" /etc/system.sh
   sed -i -e "s/wifi_PASSWORD_A='password'/wifi_PASSWORD_A=${input_str_WIFI_PASSWD_A}/g" /etc/system.sh
   sed -i -e "s/WIFI_SSID_B='SSID_B'/WIFI_SSID_B=${input_str_WIFI_SSID_B}/g" /etc/system.sh
-  sed -i -e "s/wifi_PASSWORD_B='password'/wifi_PASSWORD_B=${input_str_WIFI_PASSWD_B}/g" //etc/system.sh;    
+  sed -i -e "s/wifi_PASSWORD_B='password'/wifi_PASSWORD_B=${input_str_WIFI_PASSWD_B}/g" //etc/system.sh  
+  sed -i -e "s/WIFI_SSID_B='SSID_B'/WIFI_SSID_B=${input_str_WIFI_SSID_C}/g" /etc/system.sh
+  sed -i -e "s/wifi_PASSWORD_B='password'/wifi_PASSWORD_B=${input_str_WIFI_PASSWD_C}/g" //etc/system.sh  
   sh /etc/system.sh 2> /dev/null
   read -p " 何かキーを押してデバイスを再起動してください"
   reboot
