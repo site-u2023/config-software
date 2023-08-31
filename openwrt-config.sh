30f1c0cb3f13f39c87cc8d86fa7d883edf45c2c8
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-08-31 16:16:03 +0900

    Update openwrt-config.sh

--- openwrt-config.sh
+++ openwrt-config.sh
@@ -76,7 +76,7 @@ AVAILABLE_FLASH=`df | fgrep 'overlayfs:/overlay' | awk '{ print $4 }'`
   echo -e " \033[1;31m[a]\033[0;39m": アクセスポイント（Dumb）設定
   echo -e " \033[1;37m[r]\033[0;39m": スクリプトリムーブ後終了
   echo -e " \033[7;40m[q]\033[0;39m": 終了
-  echo -e " \033[1;37m------------------------------------- August 27, 2023 Ver.2.9.4\033[0;39m"
+  echo -e " \033[1;37m------------------------------------- August 27, 2023 Ver.2.9.5\033[0;39m"
   read -p " キーを選択してください [s/i/p/a or r/q]: " num
   case "${num}" in
     "s" ) _func_SYSTEM ;;
