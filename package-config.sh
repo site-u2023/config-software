666573d6d2ce936f0b1430339aeb1dc430919587
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-08-31 21:37:57 +0900

    Update package-config.sh

--- package-config.sh
+++ package-config.sh
@@ -220,15 +220,16 @@ grep -vixf /etc/config-software/list-installed/schedule /etc/config-software/lis
 {
 if [ ! -s $`cat /etc/config-software/list-installed/installed` ]; then
 echo -e " \033[1;37mインストールは成功しました\033[0;39m"
-else
-echo -e " \033[1;37m失敗したインストール:"
-echo -e "\033[1;37m`cat /etc/config-software/list-installed/installed`\033[0;39m"
-echo -e " \033[1;37mインストールに失敗したパッケージは再度スクリプトを回して取得ください\033[0;39m"
-fi
-}
 read -p " 何かキーを押してデバイスを再起動してください"
 reboot
 exit
+else
+echo -e " \033[1;37m失敗したインストール:"
+echo -e "\033[1;37m`cat /etc/config-software/list-installed/installed`\033[0;39m"
+echo -e " \033[1;37m何かキーを押すと再度スクリプトを回します033[0;39m"
+_func_INSTALL
+fi
+}
 }
 
 while :
