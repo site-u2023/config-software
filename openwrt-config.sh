5e57456838cf21bf6183b17e8854ed6dfd099747
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-03 01:32:48 +0900

    Update openwrt-config.sh

--- openwrt-config.sh
+++ openwrt-config.sh
@@ -53,21 +53,21 @@ do
   esac
 done
 
+function _func_AD_DNS
 while :
 do
-cat << EOF > /etc/config-software/config-banner
-  _______                     ________        __
- |       |.-----.-----.-----.|  |  |  |.----.|  |_
- |   -   ||  _  |  -__|     ||  |  |  ||   _||   _|
- |_______||   __|_____|__|__||________||__|  |____|
-          |__| W I R E L E S S   F R E E D O M
- -----------------------------------------------------
- OpenWrt-CONFIG 02.11.2
- -----------------------------------------------------
-EOF
-echo -e " \033[1;37m`cat /etc/config-software/config-banner`\033[0;39m"
-echo -e " \033[1;37m自動で切り替わります\033[0;39m" 
-sleep 1
+  echo -e " \033[1;35mDNS暗号化・広告ブロッカーインストールのスクリプトをダウンロードします\033[0;39m"
+  read -p " 宜しいですか? [y/n]: " num
+  case "${num}" in
+    "y" ) wget --no-check-certificate -O /etc/config-software/ad-dns-blocking-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/ad-dns-blocking-config.sh
+          sh /etc/config-software/ad-dns-blocking-config.sh
+          break ;;
+    "n" ) break ;;
+  esac
+done
+
+while :
+do
 {
 OPENWRT_RELEAS=`grep -o '[0-9]*' /etc/openwrt_version`
 if [ "${OPENWRT_RELEAS:0:2}" = "23" ] || [ "${OPENWRT_RELEAS:0:2}" = "21" ] || [ "${OPENWRT_RELEAS:0:2}" = "22" ]; then
@@ -77,26 +77,34 @@ if [ "${OPENWRT_RELEAS:0:2}" = "23" ] || [ "${OPENWRT_RELEAS:0:2}" = "21" ] || [
  exit
 fi
 }
-AVAILABLE_MEMORY=`free | fgrep 'Mem:' | awk '{ print $4 }'`
-AVAILABLE_FLASH=`df | fgrep 'overlayfs:/overlay' | awk '{ print $4 }'`
+  echo -e " \033[1;37m-----------------------------------------------------\033[0;39m"
+  echo -e " \033[1;37mOpenWrt-CONFIG 03.0.2\033[0;39m"
+  echo -e " \033[1;37m-----------------------------------------------------\033[0;39m"
+  echo -e " \033[1;37m自動で切り替わります\033[0;39m" 
+  sleep 0.5
+  AVAILABLE_MEMORY=`free | fgrep 'Mem:' | awk '{ print $4 }'`
+  AVAILABLE_FLASH=`df | fgrep 'overlayfs:/overlay' | awk '{ print $4 }'`
   echo -e " \033[1;37mスクリプト設置ディレクトリ: /etc/config-software\033[0;39m" 
   echo -e " \033[1;37m推奨設定順序: システム > インターネット > パッケージ\033[0;39m" 
   echo -e " \033[1;37mopenwrt-config --------------------------------------\033[0;39m"
   echo -e " \033[1;34m[s]\033[0;39m": システム初期設定
-  echo -e " \033[1;32m[i]\033[0;39m": インターネット接続設定
-  echo -e " \033[1;35m[p]\033[0;39m": パッケージインストール（USB自動判別）
-  echo -e " \033[1;31m[a]\033[0;39m": アクセスポイント（Dumb）設定
-  echo -e " \033[1;37m[r]\033[0;39m": スクリプトリムーブ後終了
+  echo -e " \033[1;33m[i]\033[0;39m": インターネット接続設定
+  echo -e " \033[1;32m[p]\033[0;39m": パッケージインストール（USB自動判別）
+  echo -e " \033[1;35m[a]\033[0;39m": アクセスポイント（Dumb）設定
+  echo -e " \033[1;31m[d]\033[0;39m": DNS暗号化・広告ブロッカーインストール
+  echo -e " \033[1;36m[r]\033[0;39m": スクリプトリムーブ後終了
   echo -e " \033[7;40m[q]\033[0;39m": 終了
   echo -e " \033[1;37m------------------------------------- August 27, 2023\033[0;39m"
-  read -p " キーを選択してください [s/i/p/a or r/q]: " num
+  read -p " キーを選択してください [s/i/p/a/d or r/q]: " num
   case "${num}" in
     "s" ) _func_SYSTEM ;;
     "i" ) _func_INTERNET ;;
     "p" ) _func_PACKAGE ;;
     "a" ) _func_ACCESSPOINT ;;
+    "d" ) _func_AD_DNS ;;
     "r" ) rm -rf /etc/config-software
           echo -e " \033[1;31mこのスクリプト及び関連スクリプトをリムーブ後終了しました\033[0;39m"
+          echo -e " \033[1;31m復元用ファイルは削除されていません\033[0;39m"
           exit ;;
     "q" ) echo -e " \033[1;31m終了しました\033[0;39m"
           exit ;;
