ec512ffa084e77ca0d80fbb5d2f7c26f3df30049
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-03 21:40:57 +0900

    Update package.sh

--- package.sh
+++ package.sh
@@ -165,7 +165,7 @@ rm /tmp/luci-app-internet-detector_1.0-1_all.ipk
 fi
 
 opkg list-installed | awk '{ print $1 }' > /etc/config-software/list-installed/After
-grep -svixf /etc/config-software/list-installed/before /etc/config-software/list-installed/After > /etc/config-software/list-installed/Difference
+grep -vixfs /etc/config-software/list-installed/before /etc/config-software/list-installed/After > /etc/config-software/list-installed/Difference
 if [ ! -s $`cat /etc/config-software/list-installed/Difference` ]; then
  echo -e " \033[1;37mインストールは完了しました\033[0;39m"
  read -p " 何かキーを押してデバイスを再起動してください"
@@ -401,7 +401,7 @@ if [ -z "$DETECTER" ]; then
 fi
 
 opkg list-installed | awk '{ print $1 }' > /etc/config-software/list-installed/After
-grep -svixf /etc/config-software/list-installed/before /etc/config-software/list-installed/After > /etc/config-software/list-installed/Difference
+grep -vixfs /etc/config-software/list-installed/before /etc/config-software/list-installed/After > /etc/config-software/list-installed/Difference
 if [ ! -s $`cat /etc/config-software/list-installed/Difference` ]; then
 echo -e " \033[1;37mインストールはありません\033[0;39m"
 read -p " 何かキーを押して終了して下さい"
