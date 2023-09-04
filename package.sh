4646f63943916646cfd4528e77c08fca5f59aa0a
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-04 22:35:24 +0900

    Update package.sh

--- package.sh
+++ package.sh
@@ -172,9 +172,9 @@ fi
  
 opkg list-installed | awk '{ print $1 }' > /etc/config-software/list-installed/After
 awk -F, 'FNR==NR{a[$1]++; next} !a[$1]' /etc/config-software/list-installed/After /etc/config-software/list-installed/Before > /etc/config-software/list-installed/Difference
-if [ -s /etc/config-software/list-installed/Difference ]; then
 while :
 do
+if [ -s /etc/config-software/list-installed/Difference ]; then
  echo -e "\033[1;33m`cat /etc/config-software/list-installed/Difference`\033[0;39m"
  echo -e " \033[1;31mインストールに失敗しました\033[0;39m"
  read -p " インストールを再試行します [y/n]: " num
@@ -185,8 +185,8 @@ else
  echo -e " \033[1;36mインストールが完了しました\033[0;39m"
  read -p " 何かキーを押してデバイスを再起動してください"
  reboot
-done
 fi
+done
 
 }
 
