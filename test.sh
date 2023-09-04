fbac7c1da828311d45fd1bae6fe662aff9b77e9d
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-04 13:30:52 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,183 @@
 #! /bin/sh
 # OpenWrt >= 21.02:
 
+function _func_PACKAGE_INSTALL {
 
+# アップデート
+if [ ! -e ${UPDATE} ]; then
+opkg update
+fi
+
+# LuCi
+if [ -z "$LUCI" ]; then
+opkg install luci
+fi
+
+# LuCi日本語化
+if [ -z "$LUCI_JA" ]; then
+opkg install luci-i18n-base-ja
+fi
+if [ -z "$LUCI_JA_OPKG" ]; then
+opkg install luci-i18n-opkg-ja
+fi
+if [ -z "$LUCI_JA_FIREWALL" ]; then
+opkg install luci-i18n-firewall-ja
+fi
+
+# SFTPサーバー
+if [ -z "$SFTP" ]; then
+opkg install openssh-sftp-server
+fi
+
+# CPU負荷分散
+if [ -z "$CPU" ]; then
+opkg install irqbalance
+uci set irqbalance.irqbalance=irqbalance
+uci set irqbalance.irqbalance.enabled='1'
+uci commit irqbalance
+fi
+
+# SQM
+if [ -z "$SQM" ]; then
+opkg install sqm-scripts
+DOWNLOAD='0' #初期値
+UPLOAD='0' #初期値
+. /lib/functions/network.sh
+network_flush_cache
+network_find_wan6 NET_IF6
+network_get_physdev NET_L2D6 "${NET_IF6}"
+uci set sqm.@queue[0].enabled='1'
+uci set sqm.@queue[0].interface=${NET_L2D6}
+uci set sqm.@queue[0].download=${DOWNLOAD}
+uci set sqm.@queue[0].upload=${UPLOAD}
+uci commit sqm
+/etc/init.d/sqm start
+/etc/init.d/sqm enable
+fi
+if [ -z "$SQM_APP" ]; then
+opkg install luci-app-sqm
+fi
+if [ -z "$SQM_APP_JA" ]; then
+opkg install luci-i18n-sqm-ja
+fi
+
+# ネットワーク統計インターフェイス
+if [ -z "$STATUS" ]; then
+opkg install luci-app-statistics
+fi
+if [ -z "$STATUS_JA" ]; then
+opkg install luci-i18n-statistics-ja
+fi
+
+# 帯域幅モニター
+if [ -z "$NLBWMON" ]; then
+opkg install nlbwmon
+fi
+if [ -z "$NLBWMON_JA" ]; then
+opkg install luci-app-nlbwmon
+fi
+if [ -z "$NLBWMON_JA" ]; then
+opkg install luci-i18n-nlbwmon-ja
+fi
+
+# WiFiスケジュール
+if [ -z "$WIFISCHEDULE" ]; then
+opkg install wifischedule
+fi
+if [ -z "$WIFISCHEDULE_APP" ]; then
+opkg install luci-app-wifischedule
+fi
+if [ -z "$WIFISCHEDULE_APP_JA" ]; then
+opkg install luci-i18n-wifischedule-ja
+fi
+
+# 追加テーマ
+if [ -z "$THEME_OPENWRT" ]; then
+opkg install luci-theme-openwrt
+fi
+if [ -z "$THEME_MATERIAL" ]; then
+opkg install luci-theme-material
+fi
+if [ -z "$THEME_2020" ]; then
+opkg install luci-theme-openwrt-2020
+fi
+
+# カスタムフィード
+
+# ログ情報
+if [ -z "$LOG" ]; then
+wget --no-check-certificate -O /tmp/luci-app-log_0.6-2_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-app-log_0.6-2_all.ipk
+opkg install /tmp/luci-app-log_0.6-2_all.ipk
+rm /tmp/luci-app-log_0.6-2_all.ipk
+fi
+
+# CPUステータス
+if [ -z "$CPU_STATUS" ]; then
+wget --no-check-certificate -O /tmp/luci-app-log_0.6-2_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-app-log_0.6-2_all.ipk
+opkg install /tmp/luci-app-log_0.6-2_all.ipk
+rm /tmp/luci-app-log_0.6-2_all.ipk
+fi
+
+# CPUパフォーマンス
+if [ -z "$CPU_PERFORMANCE" ]; then
+OPENWRT_RELEAS=`grep -o '[0-9]*' /etc/openwrt_version`
+ if [ "${OPENWRT_RELEAS:0:2}" = "23" ]; then
+ wget --no-check-certificate -O /tmp/luci-app-cpu-perf_0.4-0_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/23.05/luci-app-cpu-perf_0.4-0_all.ipk opkg install /tmp/luci-app-cpu-perf_0.4-0_all.ipk
+ rm /tmp/luci-app-cpu-perf_0.4-0_all.ipk
+ /etc/init.d/cpu-perf start
+ else
+ wget --no-check-certificate -O /tmp/luci-app-cpu-perf_0.4-0_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-app-cpu-perf_0.4-0_all.ipk
+ opkg install /tmp/luci-app-cpu-perf_0.4-0_all.ipk
+ rm /tmp/luci-app-cpu-perf_0.4-0_all.ipk
+ /etc/init.d/cpu-perf start
+ fi
+fi
+
+# 温度センサー
+if [ -z "$TMP_STATUS" ]; then
+ OPENWRT_RELEAS=`grep -o '[0-9]*' /etc/openwrt_version`
+ if [ "${OPENWRT_RELEAS:0:2}" = "23" ]; then
+ wget --no-check-certificate -O /tmp/luci-app-temp-status_0.3-5_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/23.05/luci-app-temp-status_0.3-5_all.ipk
+ opkg install /tmp/luci-app-temp-status_0.3-5_all.ipk
+ rm /tmp/luci-app-temp-status_0.3-5_all.ipk
+ else
+ wget --no-check-certificate -O /tmp/luci-app-temp-status_0.3-5_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-app-temp-status_0.3-5_all.ipk
+ opkg install /tmp/luci-app-temp-status_0.3-5_all.ipk
+ rm /tmp/luci-app-temp-status_0.3-5_all.ipk
+ fi
+fi
+
+# インターネット可用性確認
+if [ -z "$DETECTER" ]; then
+wget --no-check-certificate -O /tmp/internet-detector_1.0-3_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/internet-detector_1.0-3_all.ipk
+opkg install /tmp/internet-detector_1.0-3_all.ipk
+rm /tmp/internet-detector_1.0-3_all.ipk
+/etc/init.d/internet-detector start
+/etc/init.d/internet-detector enable
+fi
+if [ -z "$DETECTER_APP" ]; then
+wget --no-check-certificate -O /tmp/luci-app-internet-detector_1.0-1_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-app-internet-detector_1.0-1_all.ipk
+opkg install /tmp/luci-app-internet-detector_1.0-1_all.ipk
+rm /tmp/luci-app-internet-detector_1.0-1_all.ipk
+fi
+
+opkg list-installed | awk '{ print $1 }' > /etc/config-software/list-installed/After
+diff -u /etc/config-software/list-installed/Before /etc/config-software/list-installed/After | grep ^+ | grep -v ^+++ | sed s/^+// > /etc/config-software/list-installed/Difference
+if [ ! -s $`cat /etc/config-software/list-installed/Difference` ]; then
+ echo -e " \033[1;37mインストールは完了しました\033[0;39m"
+ read -p " 何かキーを押してデバイスを再起動してください"
+ reboot
+else
+ echo -e " \033[1;37m失敗したインストール\033[0;39m"
+ echo -e "\033[1;37m`cat /etc/config-software/list-installed/Difference`\033[0;39m"
+ echo -e " \033[1;37m失敗したインストールを再試行します\033[0;39m"
+ read -p " 何かキーを押すと再度スクリプトを開始します"
+ _func_PACKAGE_INSTALL
+fi
+
+}
+
+# ----------------------------------------------------------------
 
 
 function _func_listinstalled_Before {
@@ -12,7 +188,7 @@ _func_luci
 
 function _func_luci {
 LUCI=`opkg list-installed luci | awk '{ print $1 }'`
-if [ -n "$LUCI" ]; then
+if [ -z "$LUCI" ]; then
 while :
 do
   echo -e " \033[1;33mLuciをインストールしますか\033[0;39m"
@@ -345,7 +521,7 @@ do
   echo -e "\033[1;37m`cat /etc/config-software/list-installed/Difference`\033[0;39m"
   read -p " インストールを開始します [y/n or q]: " num
    case "${num}" in
-    "y" ) ;;
+    "y" ) _func_PACKAGE_INSTALL ;;
     "n" ) _func_listinstalled_Before ;;
     "q" ) exit ;; 
   esac
