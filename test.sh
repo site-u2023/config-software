a33253b02d2dd5b67d4d7a98904f03f35f5b46f5
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-04 13:04:30 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,191 +2,15 @@
 # OpenWrt >= 21.02:
 
 
-function _func_PACKAGE_INSTALL {
-
-# アップデート
-if [ ! -e ${UPDATE} ]; then
-opkg update
-fi
 
 
-# LuCi
-if [ -z "$LUCI" ]; then
-opkg install luci
-fi
-
-# LuCi日本語化
-if [ -z "$LUCI_JA" ]; then
-opkg install luci-i18n-base-ja
-fi
-if [ -z "$LUCI_JA_OPKG" ]; then
-opkg install luci-i18n-opkg-ja
-fi
-if [ -z "$LUCI_JA_FIREWALL" ]; then
-opkg install luci-i18n-firewall-ja
-fi
-
-# SFTPサーバー
-if [ -z "$SFTP" ]; then
-opkg install openssh-sftp-server
-fi
-
-# CPU負荷分散
-if [ -z "$CPU" ]; then
-opkg install irqbalance
-uci set irqbalance.irqbalance=irqbalance
-uci set irqbalance.irqbalance.enabled='1'
-uci commit irqbalance
-fi
-
-# SQM
-if [ -z "$SQM" ]; then
-opkg install sqm-scripts
-DOWNLOAD='0' #初期値
-UPLOAD='0' #初期値
-. /lib/functions/network.sh
-network_flush_cache
-network_find_wan6 NET_IF6
-network_get_physdev NET_L2D6 "${NET_IF6}"
-uci set sqm.@queue[0].enabled='1'
-uci set sqm.@queue[0].interface=${NET_L2D6}
-uci set sqm.@queue[0].download=${DOWNLOAD}
-uci set sqm.@queue[0].upload=${UPLOAD}
-uci commit sqm
-/etc/init.d/sqm start
-/etc/init.d/sqm enable
-fi
-if [ -z "$SQM_APP" ]; then
-opkg install luci-app-sqm
-fi
-if [ -z "$SQM_APP_JA" ]; then
-opkg install luci-i18n-sqm-ja
-fi
-
-# ネットワーク統計インターフェイス
-if [ -z "$STATUS" ]; then
-opkg install luci-app-statistics
-fi
-if [ -z "$STATUS_JA" ]; then
-opkg install luci-i18n-statistics-ja
-fi
-
-# 帯域幅モニター
-if [ -z "$NLBWMON" ]; then
-opkg install nlbwmon
-fi
-if [ -z "$NLBWMON_JA" ]; then
-opkg install luci-app-nlbwmon
-fi
-if [ -z "$NLBWMON_JA" ]; then
-opkg install luci-i18n-nlbwmon-ja
-fi
-
-# WiFiスケジュール
-if [ -z "$WIFISCHEDULE" ]; then
-opkg install wifischedule
-fi
-if [ -z "$WIFISCHEDULE_APP" ]; then
-opkg install luci-app-wifischedule
-fi
-if [ -z "$WIFISCHEDULE_APP_JA" ]; then
-opkg install luci-i18n-wifischedule-ja
-fi
-
-# 追加テーマ
-if [ -z "$THEME_OPENWRT" ]; then
-opkg install luci-theme-openwrt
-fi
-if [ -z "$THEME_MATERIAL" ]; then
-opkg install luci-theme-material
-fi
-if [ -z "$THEME_2020" ]; then
-opkg install luci-theme-openwrt-2020
-fi
-
-# カスタムフィード
-
-# ログ情報
-if [ -z "$LOG" ]; then
-wget --no-check-certificate -O /tmp/luci-app-log_0.6-2_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-app-log_0.6-2_all.ipk
-opkg install /tmp/luci-app-log_0.6-2_all.ipk
-rm /tmp/luci-app-log_0.6-2_all.ipk
-fi
-
-# CPUステータス
-if [ -z "$CPU_STATUS" ]; then
-wget --no-check-certificate -O /tmp/luci-app-log_0.6-2_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-app-log_0.6-2_all.ipk
-opkg install /tmp/luci-app-log_0.6-2_all.ipk
-rm /tmp/luci-app-log_0.6-2_all.ipk
-fi
-
-# CPUパフォーマンス
-if [ -z "$CPU_PERFORMANCE" ]; then
-OPENWRT_RELEAS=`grep -o '[0-9]*' /etc/openwrt_version`
- if [ "${OPENWRT_RELEAS:0:2}" = "23" ]; then
- wget --no-check-certificate -O /tmp/luci-app-cpu-perf_0.4-0_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/23.05/luci-app-cpu-perf_0.4-0_all.ipk opkg install /tmp/luci-app-cpu-perf_0.4-0_all.ipk
- rm /tmp/luci-app-cpu-perf_0.4-0_all.ipk
- /etc/init.d/cpu-perf start
- else
- wget --no-check-certificate -O /tmp/luci-app-cpu-perf_0.4-0_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-app-cpu-perf_0.4-0_all.ipk
- opkg install /tmp/luci-app-cpu-perf_0.4-0_all.ipk
- rm /tmp/luci-app-cpu-perf_0.4-0_all.ipk
- /etc/init.d/cpu-perf start
- fi
-fi
-
-# 温度センサー
-if [ -z "$TMP_STATUS" ]; then
- OPENWRT_RELEAS=`grep -o '[0-9]*' /etc/openwrt_version`
- if [ "${OPENWRT_RELEAS:0:2}" = "23" ]; then
- wget --no-check-certificate -O /tmp/luci-app-temp-status_0.3-5_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/23.05/luci-app-temp-status_0.3-5_all.ipk
- opkg install /tmp/luci-app-temp-status_0.3-5_all.ipk
- rm /tmp/luci-app-temp-status_0.3-5_all.ipk
- else
- wget --no-check-certificate -O /tmp/luci-app-temp-status_0.3-5_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-app-temp-status_0.3-5_all.ipk
- opkg install /tmp/luci-app-temp-status_0.3-5_all.ipk
- rm /tmp/luci-app-temp-status_0.3-5_all.ipk
- fi
-fi
-
-# インターネット可用性確認
-if [ -z "$DETECTER" ]; then
-wget --no-check-certificate -O /tmp/internet-detector_1.0-3_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/internet-detector_1.0-3_all.ipk
-opkg install /tmp/internet-detector_1.0-3_all.ipk
-rm /tmp/internet-detector_1.0-3_all.ipk
-/etc/init.d/internet-detector start
-/etc/init.d/internet-detector enable
-fi
-if [ -z "$DETECTER_APP" ]; then
-wget --no-check-certificate -O /tmp/luci-app-internet-detector_1.0-1_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-app-internet-detector_1.0-1_all.ipk
-opkg install /tmp/luci-app-internet-detector_1.0-1_all.ipk
-rm /tmp/luci-app-internet-detector_1.0-1_all.ipk
-fi
-
-opkg list-installed | awk '{ print $1 }' > /etc/config-software/list-installed/After
-diff -u /etc/config-software/list-installed/Before /etc/config-software/list-installed/After | grep ^+ | grep -v ^+++ | sed s/^+// > /etc/config-software/list-installed/Difference
-if [ ! -s $`cat /etc/config-software/list-installed/Difference` ]; then
- echo -e " \033[1;37mインストールは完了しました\033[0;39m"
- read -p " 何かキーを押してデバイスを再起動してください"
- reboot
-else
- echo -e " \033[1;37m失敗したインストール\033[0;39m"
- echo -e "\033[1;37m`cat /etc/config-software/list-installed/Difference`\033[0;39m"
- echo -e " \033[1;37m失敗したインストールを再試行します\033[0;39m"
- read -p " 何かキーを押すと再度スクリプトを開始します"
- _func_PACKAGE_INSTALL
-fi
-
-}
-
-# ----------------------------------------------------------------
-
-function _func_PACKAGE_SELECTOR {
+function _func_list-installed_Before {
 mkdir -p /etc/config-software/list-installed
 opkg list-installed | awk '{ print $1 }' > /etc/config-software/list-installed/Before
+_func_luci
 }
 
-# LuCiA
+function _func_luci {
 LUCI=`opkg list-installed luci | awk '{ print $1 }'`
 if [ -n "$LUCI" ]; then
 while :
@@ -202,8 +26,10 @@ do
   esac
 done
 fi
+_func_luci-i18n-base-ja
+}
 
-# LuCi日本語化
+function _func_luci-i18n-base-ja {
 LUCI_JA=`opkg list-installed luci-i18n-base-ja | awk '{ print $1 }'`
 LUCI_JA_OPKG=`opkg list-installed luci-i18n-opkg-ja | awk '{ print $1 }'`
 LUCI_JA_FIREWALL=`opkg list-installed luci-i18n-firewall-ja | awk '{ print $1 }'`
@@ -225,8 +51,10 @@ do
   esac
 done
 fi
+_func_openssh-sftp-server
+}
 
-# SFTPサーバー
+function _func_openssh-sftp-server {
 SFTP=`opkg list-installed openssh-sftp-server | awk '{ print $1 }'`
 if [ -z "$SFTP" ]; then
 while :
@@ -242,8 +70,10 @@ do
   esac
 done
 fi
+_func_irqbalance
+}
 
-# CPU負荷分散
+function _func_irqbalance {
 CPU=`opkg list-installed irqbalance | awk '{ print $1 }'`
 while :
 do
@@ -263,8 +93,10 @@ do
     esac
 done
 fi
+_func_sqm-scripts
+}
 
-# SQM
+function _func_sqm-scripts {
 SQM=`opkg list-installed sqm-scripts | awk '{ print $1 }'`
 SQM_APP=`opkg list-installed luci-app-sqm | awk '{ print $1 }'`
 SQM_APP_JA=`opkg list-installed luci-i18n-sqm-ja | awk '{ print $1 }'`
@@ -286,8 +118,10 @@ do
   esac
 done
 fi
+_func_luci-app-statistics
+}
 
-# ネットワーク統計インターフェイス
+function _func_luci-app-statistics {
 STATUS=`opkg list-installed luci-app-statistics | awk '{ print $1 }'`
 STATUS_JA=`opkg list-installed luci-i18n-statistics-ja | awk '{ print $1 }'`
 if [ -z "$STATUS" ]; then
@@ -306,8 +140,10 @@ do
   esac
 done
 fi
+_func_nlbwmon
+}
 
-# 帯域幅モニター
+function _func_nlbwmon {
 NLBWMON=`opkg list-installed nlbwmon | awk '{ print $1 }'`
 NLBWMON_JA=`opkg list-installed luci-app-nlbwmon | awk '{ print $1 }'`
 NLBWMON_JA=`opkg list-installed luci-i18n-nlbwmon-ja | awk '{ print $1 }'`
@@ -328,8 +164,10 @@ do
   esac
 done
 fi
+_func_wifischedule
+}
 
-# WiFiスケジュール
+function _func_wifischedule {
 WIFISCHEDULE=`opkg list-installed wifischedule | awk '{ print $1 }'`
 WIFISCHEDULE_APP=`opkg list-installed luci-app-wifischedule | awk '{ print $1 }'`
 WIFISCHEDULE_APP_JA=`opkg list-installed luci-i18n-wifischedule-ja | awk '{ print $1 }'`
@@ -351,8 +189,10 @@ do
   esac
 done
 fi
+_func_luci-theme-openwrt
+}
 
-# 追加テーマ
+function _func_luci-theme-openwrt {
 THEME_OPENWRT=`opkg list-installed luci-theme-openwrt | awk '{ print $1 }'`
 THEME_MATERIAL=`opkg list-installed luci-theme-material | awk '{ print $1 }'`
 THEME_2020=`opkg list-installed luci-theme-openwrt-2020 | awk '{ print $1 }'`
@@ -374,8 +214,10 @@ do
   esac
 done
 fi
+_func_luci-app-log
+}
 
-# ログ情報
+function _func_luci-app-log {
 LOG=`opkg list-installed luci-app-log | awk '{ print $1 }'`
 if [ -z "$LOG" ]; then
 while :
@@ -392,8 +234,10 @@ do
   esac
 done
 fi
+_func_luci-app-cpu-status
+}
 
-# CPUステータス
+function _func_luci-app-cpu-status {
 CPU_STATUS=`opkg list-installed luci-app-cpu-status | awk '{ print $1 }'`
 if [ -z "$CPU_STATUS" ]; then
 while :
@@ -410,8 +254,10 @@ do
   esac
 done
 fi
+_func_luci-app-cpu-perf
+}
 
-# CPUパフォーマンス
+function _func_luci-app-cpu-perf {
 CPU_PERFORMANCE=`opkg list-installed luci-app-cpu-perf | awk '{ print $1 }'`
 if [ -z "$CPU_PERFORMANCE" ]; then
 while :
@@ -428,8 +274,10 @@ do
   esac
 done
 fi
+_func_luci-app-temp-status
+}
 
-# 温度センサー
+function _func_luci-app-temp-status {
 TMP_STATUS=`opkg list-installed luci-app-temp-status | awk '{ print $1 }'`
 if [ -z "$TMP_STATUS" ]; then
 while :
@@ -446,8 +294,10 @@ do
   esac
 done
 fi
+_func_internet-detector
+}
 
-# インターネット可用性確認
+function _func_internet-detector {
 DETECTER=`opkg list-installed internet-detector | awk '{ print $1 }'`
 DETECTER_APP=`opkg list-installed luci-app-internet-detector | awk '{ print $1 }'`
 if [ -z "$DETECTER" ]; then
@@ -467,8 +317,10 @@ do
   esac
 done
 fi
+_func_diffutils
+}
 
-# DIFF
+function _func_diffutils {
 DIFF=`opkg list-installed diffutils | awk '{ print $1 }'`
 UPDATE="/tmp/opkg-lists/openwrt_telephony.sig"
 if [ -z "$DIFF" ]; then
@@ -477,7 +329,10 @@ if [ -z "$DIFF" ]; then
  fi
 opkg install diffutils
 fi
+_func_list-installed_After
+}
 
+function _func_list-installed_After {
 opkg list-installed | awk '{ print $1 }' > /etc/config-software/list-installed/After
 diff -u /etc/config-software/list-installed/Before /etc/config-software/list-installed/After | grep ^+ | grep -v ^+++ | sed s/^+// > /etc/config-software/list-installed/Difference
 if [ ! -s $`cat /etc/config-software/list-installed/Difference` ]; then
@@ -491,16 +346,15 @@ do
   echo -e "\033[1;37m`cat /etc/config-software/list-installed/Difference`\033[0;39m"
   read -p " インストールを開始します [y/n or q]: " num
    case "${num}" in
-    "y" ) _func_PACKAGE_INSTALL ;;
-    "n" ) _func_PACKAGE_SELECTOR ;;
+    "y" ) ;;
+    "n" ) _func_list-installed_Before ;;
     "q" ) exit ;; 
   esac
 done
+}
 
 # ----------------------------------------------------------------
 
-while :
-do
 OPENWRT_RELEAS=`grep -o '[0-9]*' /etc/openwrt_version`
 if [ "${OPENWRT_RELEAS:0:2}" = "23" ] || [ "${OPENWRT_RELEAS:0:2}" = "21" ] || [ "${OPENWRT_RELEAS:0:2}" = "22" ]; then
  echo -e " \033[1;37mバージョンチェック: OK\033[0;39m"
@@ -509,10 +363,12 @@ else
  exit
 fi
 
+while :
+do
   echo -e " \033[1;35mパッケージの選択をしてからインストールを開始します\033[0;39m"
   read -p " パッケージの選択を開始します [y/n]: " num
   case "${num}" in
-    "y" ) _func_PACKAGE_SELECTOR ;;
+    "y" ) _func_list-installed_Before ;;
     "n" ) exit ;;
   esac
 done
