86cdba2c4b16d5e472c47e690aea328b895af435
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-05 22:27:52 +0900

    Update package-config.sh

--- package-config.sh
+++ package-config.sh
@@ -1,6 +1,33 @@
 #! /bin/sh
 # OpenWrt >= 21.02:
 
+
+function _func_full_INST
+while :
+do
+  echo -e " \033[1;3自動フルインストールのスクリプトをダウンロードします\033[0;39m"
+  read -p " 宜しいですか? [y/n]: " num
+  case "${num}" in
+    "y" ) wget --no-check-certificate -O /etc/config-software/package-auto.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-auto.sh
+          sh /etc/config-software/package-auto.sh
+          break ;;
+    "n" ) break ;;
+  esac
+done
+
+function _func_choice_INST
+while :
+do
+  echo -e " \033[1;32m選択インストールのスクリプトをダウンロードします\033[0;39m"
+  read -p " 宜しいですか? [y/n]: " num
+  case "${num}" in
+    "y" ) wget --no-check-certificate -O /etc/config-software/package-manual.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-manual.sh
+          sh /etc/config-software/package-manual.sh
+          break ;;
+    "n" ) break ;;
+  esac
+done
+
 OPENWRT_RELEAS=`grep -o '[0-9]*' /etc/openwrt_version`
 if [ "${OPENWRT_RELEAS:0:2}" = "23" ] || [ "${OPENWRT_RELEAS:0:2}" = "21" ] || [ "${OPENWRT_RELEAS:0:2}" = "22" ]; then
  echo -e " \033[1;32mバージョンチェック: OK\033[0;39m"
@@ -19,10 +46,8 @@ do
   echo -e " \033[1;37m------------------------------------------------------\033[0;39m"
   read -p " キーを選択してください [f/c or q]: " num 
   case "${num}" in
-    "f" ) wget --no-check-certificate -O /etc/config-software/package-auto.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-auto.sh
-          sh /etc/config-software/package-auto.sh ;;
-    "c" ) wget --no-check-certificate -O /etc/config-software/package-manual.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-manual.sh
-          sh /etc/config-software/package-manual.sh ;;
+    "f" ) _func_full_INST ;;
+    "c" ) _func_choice_INST ;;
     "q" ) exit ;;
   esac
 done
