2f01e52f42df62149d082689d5dd25629bcc306b
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-05 15:22:51 +0900

    Update adblock-config.sh

--- adblock-config.sh
+++ adblock-config.sh
@@ -105,8 +105,8 @@ exit
 
 
 if [ "adguardhome" = "`opkg list-installed adguardhome | awk '{ print $1 }'`" ]; then
-  read -p " AdGuardがインストールされている為終了します"
-  exit
+ read -p " AdGuardがインストールされている為終了します"
+ exit
 fi
 OPENWRT_RELEAS=`grep -o '[0-9]*' /etc/openwrt_version`
 if [ "${OPENWRT_RELEAS:0:2}" = "23" ] || [ "${OPENWRT_RELEAS:0:2}" = "22" ]; then
