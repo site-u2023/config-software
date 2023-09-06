2dfd1acdd52e5b1cd1b364ffe533dd70ee9ff37f
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-06 23:56:38 +0900

    Create adblock.sh

---
+++ adblock.sh
@@ -0,0 +1,31 @@
+uci set adblock.global.adb_backupdir="/etc/adblock"
+
+cp /etc/adblock/adblock.sources.gz /etc/adblock/adblock.sources.tofu.gz
+gunzip /etc/adblock/adblock.sources.tofu.gz
+
+sed -i -e '$d' /etc/adblock/adblock.sources.tofu
+sed -i -e '$d' /etc/adblock/adblock.sources.tofu
+cat <<"EOF" >> /etc/adblock/adblock.sources.tofu
+                },
+                "tofu": {
+                                "url": "https://raw.githubusercontent.com/tofukko/filter/master/Adblock_Plus_list.txt",
+                                "rule": "BEGIN{FS=\"[|^]\"}/^\\|\\|([[:alnum:]_-]{1,63}\\.)+[[:alpha:]]+\\^(\\$third-party)?$/{print tolower($3)}",
+                                "size": "XL",
+                                "focus": "tofu",
+                                "descurl": "https://github.com/tofukko/filter"
+                }
+}
+EOF
+
+gzip /etc/adblock/adblock.sources.tofu
+
+uci set adblock.global.adb_srcarc="/etc/adblock/adblock.sources.tofu.gz"
+uci set adblock.global.adb_enabled="1"
+uci set adblock.global.adb_backup="1"
+uci set adblock.global.adb_backupdir="/etc/adblock"
+uci set adblock.global.adb_backup_mode="1"
+uci add_list adblock.global.adb_sources='tofu'
+
+uci commit adblock
+service adblock enable
+service adblock start
