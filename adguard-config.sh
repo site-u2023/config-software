8de1a3d715bdfa4577d03024707976d030817cd8
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-06 23:18:15 +0900

    Update adguard-config.sh

--- adguard-config.sh
+++ adguard-config.sh
@@ -139,14 +139,21 @@ Bcrypt_PASSWD=`htpasswd -B -n -b ${input_str_USER} ${input_str_PASSWD}`
 sed -i "6c \    password: ${Bcrypt_PASSWD#${input_str_USER}:}" /etc/adguardhome.yaml
 sed -i -e "s/280blocker_domain_ag_202309/280blocker_domain_ag_`date '+%Y%m01' | awk '{print substr($0, 1, 6)}'`/g" /etc/adguardhome.yaml
 echo -e " \033[1;32m管理用ウェブインターフェイスの設定が完了しました\033[0;39m"
+
+if [ "adguardhome" = "`opkg list-installed adguardhome | awk '{ print $1 }'`" ]; then
+  echo -e " \033[1;37mAdGuardがインストールが既にインストールされています\033[0;39m"
+  service adguardhome restart
+  exit
+else  
 read -p " 何かキーを押してインストールを開始して下さい"
 wget --no-check-certificate -O /etc/config-software/adguard.sh https://raw.githubusercontent.com/site-u2023/config-software/main/adguard.sh
 sh /etc/config-software/adguard.sh
-echo -e " \033[1;32mインストールと設定が完了しました\033[0;39m"
+echo -e " \033[1;32mインストールと設定が完了しました\033[0;39m
 echo -e " \033[1;32m管理用ウェブインターフェイス: http://${NET_ADDR}:${input_str_PORT}\033[0;39m"
 read -p " 何かキーを押してデバイスを再起動してください"
 reboot
 exit
+fi
 }
 
 function _func_AdGuard_Before
