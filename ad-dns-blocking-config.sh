4c32e9db700887aec4a77f043b80da0262c7dcee
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-07 19:50:34 +0900

    Update ad-dns-blocking-config.sh

--- ad-dns-blocking-config.sh
+++ ad-dns-blocking-config.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 
-function _func_AdGuard_INST
+function _func_AdGuard_INST {
 while :
 do
   echo -e " \033[1;34mAdGuard設定のスクリプトをダウンロードします\033[0;39m"
@@ -13,8 +13,9 @@ do
     "n" ) break ;;
   esac
 done
+}
 
-function _func_AdBlock_INST
+function _func_AdBlock_INST {
 while :
 do
   echo -e " \033[1;32mAdBlock設定のスクリプトをダウンロードします\033[0;39m"
@@ -26,6 +27,35 @@ do
     "n" ) break ;;
   esac
 done
+}
+
+function _func_DoH {
+while :
+do
+  echo -e " \033[1;32mDNS over HTTPS (DoH)設定のスクリプトをダウンロードします\033[0;39m"
+  read -p " 宜しいですか? [y/n]: " num
+  case "${num}" in
+    "y" ) wget --no-check-certificate -O /etc/config-software/doh.sh https://raw.githubusercontent.com/site-u2023/config-software/main/doh.sh
+          sh /etc/config-software/doh.sh
+          break ;;
+    "n" ) break ;;
+  esac
+done
+}
+
+function _func_DoT  {
+while :
+do
+  echo -e " \033[1;32mDNS over TLS (DoT)設定のスクリプトをダウンロードします\033[0;39m"
+  read -p " 宜しいですか? [y/n]: " num
+  case "${num}" in
+    "y" ) wget --no-check-certificate -O /etc/config-software/dot.sh https://raw.githubusercontent.com/site-u2023/config-software/main/dot.sh
+          sh /etc/config-software/dot.sh
+          break ;;
+    "n" ) break ;;
+  esac
+done
+}
 
 while :
 do
@@ -39,15 +69,22 @@ if [ "${OPENWRT_RELEAS:0:2}" = "23" ] || [ "${OPENWRT_RELEAS:0:2}" = "21" ] || [
 fi
 }
   echo -e " \033[1;37mAdGuardとAdBlockは排他利用\033[0;39m"
+  echo -e " \033[1;37mAdGuardとDNS over HTTPS (DoH)は排他利用\033[0;39m"
+  echo -e " \033[1;37mAdGuardとDNS over TLS (DoT)は排他利用\033[0;39m"
+  echo -e " \033[1;37mDNS over HTTPS (DoH)とDNS over TLS (DoT)\033[0;39m"
   echo -e " \033[1;37mad dns blocking config ------------------------------\033[0;39m"
   echo -e " \033[1;34m[g]\033[0;39m": AdGuardの設定とインストール（280blockerアドイン）
   echo -e " \033[1;32m[b]\033[0;39m": AdBlockdのインストールと設定（豆腐フィルタアドイン）
+  echo -e " \033[1;34m[h]\033[0;39m": DNS over HTTPS (DoH)のインストールと設定
+  echo -e " \033[1;32m[t]\033[0;39m": DNS over TLS (DoT)のインストールと設定
   echo -e " \033[1;31m[q]\033[0;39m": 終了
   echo -e " \033[1;37m-----------------------------------------------------\033[0;39m"
   read -p " キーを選択してください [g/b or q]: " num
   case "${num}" in
     "g" ) _func_AdGuard_INST ;;
     "b" ) _func_AdBlock_INST ;;
+    "h" ) _func_DoH ;;
+    "t" ) _func_DoT ;;
     "q" ) exit ;;
   esac
  done 
