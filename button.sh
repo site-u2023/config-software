5f5681b29d863cc17b4c0077ccabf1fc6ee7a56e
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-08 14:47:58 +0900

    Update button.sh

--- button.sh
+++ button.sh
@@ -1,21 +1,20 @@
 #! /bin/sh
 # OpenWrt >= 21.02:
 
+function _func_button_INSTALL {
+
 # install
 UPDATE="/tmp/opkg-lists/openwrt_telephony.sig"
 if [ ! -e ${UPDATE} ]; then
 opkg update
 fi
-
 opkg install kmod-button-hotplug
 
 mkdir -p /etc/hotplug.d/button
-
 # log調査用
 cat << "EOF" > /etc/hotplug.d/button/buttons
 logger "the button was ${BUTTON} and the action was ${ACTION}"
 EOF
-
 # 00-button
 cat << "EOF" > /etc/hotplug.d/button/00-button
 source /lib/functions.sh
@@ -45,17 +44,14 @@ config_load system
 config_foreach do_button button
 EOF
 
-
-# WiFi ON ワンプッシュボタンリリース
 BUTTON='wps' # AOSS BUTTON
-
-# Toggle Wi-Fi using a script (ON-OFF) 0~3s release
+# WiFi ON ワンプッシュボタンリリース
 uci add system button
 uci set system.@button[-1].button=${BUTTON}
 uci set system.@button[-1].action="released"
 uci set system.@button[-1].handler="/usr/bin/wifionoff"
 uci set system.@button[-1].min="0"
-uci set system.@button[-1].max="3"
+uci set system.@button[-1].max="2"
 uci commit system
  
 cat << "EOF" > /usr/bin/wifionoff
@@ -70,12 +66,23 @@ cat << "EOF" > /usr/bin/wifionoff
 EOF
 chmod u+x /usr/bin/wifionoff
 
+# USBストレージアンマウント 3～7秒 ボタンリリース
+if [ -n "$str_USB" ]; then
+uci add system button
+uci set system.@button[-1].button=${BUTTON}
+uci set system.@button[-1].action="released"
+uci set system.@button[-1].handler="for i in \$(mount | awk '/dev\/sd[b-z]/{print \$1}'); do umount \${i}; done"
+uci set system.@button[-1].min="3"
+uci set system.@button[-1].max="7"
+uci commit system
+fi
+
 # restart 7～13秒 ボタンリリース
 uci add system button
 uci set system.@button[-1].button=${BUTTON}
 uci set system.@button[-1].action="released"
-uci set system.@button[-1].handler="sleep 70 && touch /etc/banner && reboot"
-uci set system.@button[-1].min="7"
+uci set system.@button[-1].handler="reboot"
+uci set system.@button[-1].min="8"
 uci set system.@button[-1].max="13"
 
 # 初期化 17～23秒 ボタンリリース
@@ -83,11 +90,39 @@ uci add system button
 uci set system.@button[-1].button=${BUTTON}
 uci set system.@button[-1].action="released"
 uci set system.@button[-1].handler="firstboot && reboot now"
-uci set system.@button[-1].min="17"
+uci set system.@button[-1].min="18"
 uci set system.@button[-1].max="23"
 
 # set
 uci commit system
 /etc/init.d/system reload
 
+echo -e " \033[1;36mインストールが完了しました\033[0;39m"
+read -p " 何かキーを押してデバイスを再起動してください"
+reboot
 
+}
+
+OPENWRT_RELEAS=`grep -o '[0-9]*' /etc/openwrt_version`
+if [ "${OPENWRT_RELEAS:0:2}" = "23" ] || [ "${OPENWRT_RELEAS:0:2}" = "21" ] || [ "${OPENWRT_RELEAS:0:2}" = "22" ]; then
+ echo -e " \033[1;32mバージョンチェック: OK\033[0;39m"
+ else
+ read -p " バージョンが違うため終了します"
+ exit
+fi
+
+ while :
+ do
+  echo -e " \033[1;32m 1秒\033[0;39m" Wi-Fiオン・オフ ボタンリリース
+  str_USB=`dmesg | grep -s usb`
+  if [ -n "$str_USB" ]; then
+  echo -e " \033[1;32m 5秒\033[0;39m" USBストレージアンマウント ボタンリリース
+  fi
+  echo -e " \033[1;33m10秒\033[0;39m" デバイス再起動 ボタンリリース
+  echo -e " \033[1;31m20秒\033[0;39m" デバイス初期化 ボタンリリース
+  read -p " ボタン設定とインストール（WPSボタン用）を開始します [y/n]: " num
+  case "${num}" in
+    "y" ) _func_button_INSTALL ;;
+    "n" ) exit ;;
+  esac
+done
