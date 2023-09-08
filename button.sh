d7c2b6829142e0669cff38adc9106189d5ea823b
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-08 21:44:45 +0900

    Update button.sh

--- button.sh
+++ button.sh
@@ -61,7 +61,7 @@ cat << "EOF" > /usr/bin/wifionoff
     SW="$(uci -q get wireless.@wifi-device[0].disabled)"
     [ "${SW}" = "1" ] \
         && uci set wireless.@wifi-device[0].disabled='0'; uci set wireless.@wifi-device[1].disabled='0' \
-        || uci set wireless.@wifi-device[0].disabled='1'; uci set wireless.@wifi-device[0].disabled='1'
+        || uci set wireless.@wifi-device[0].disabled='1'; uci set wireless.@wifi-device[1].disabled='1'
     wifi
 }
 EOF
