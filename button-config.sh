70e5577100d0ed92d84a0c8bc4b059f46377dfe4
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-08 21:15:13 +0900

    Update button-config.sh

--- button-config.sh
+++ button-config.sh
@@ -5,7 +5,7 @@
 function _func_button_INST {
 while :
 do
-  echo -e " \033[1;3ボタン設定とインストール（WPSボタン用）のスクリプトをダウンロードします\033[0;39m"
+  echo -e " \033[1;34mボタン設定とインストール（WPSボタン用）のスクリプトをダウンロードします\033[0;39m"
   read -p " 宜しいですか? [y/n]: " num
   case "${num}" in
     "y" ) wget --no-check-certificate -O /etc/config-software/button.sh https://raw.githubusercontent.com/site-u2023/config-software/main/button.sh
@@ -17,14 +17,34 @@ done
 }
 
 function _func_button_investigation {
+echo -e " \033[1;37mボタン調査を開始します\033[0;39m"
+echo -e " \033[1;37m利用可能ボタン\033[0;39m"
+ls /etc/rc.button/
+echo -e ""
+echo -e " \033[1;32m`uname -n`\033[0;39m"の利用したいボタンを押して離して下さい
+echo -e ""
+read -p " ボタンを押して放したら何かキーを押して下さい"
+str=`ls /etc/rc.button/`
+str_BUTTON=`logread | tail -n 1  | grep button | awk '{ print $11 }'`
+if [ -z "$str_BUTTON" ]; then
+echo -e " \033[1;33mボタンを押して放していないか該当ボタンがありません\033[0;39m"
+read -p " 何かキーを押して終了して下さい"
+exit
+fi
+case $str in
+  *"$str_BUTTON"*)
+    echo -e " \033[1;32m${str_BUTTON}\033[0;39m"が調査したボタンです
+    echo -e " \033[1;32m${str_BUTTON}\033[0;39m"を設定します
+    ;;
+esac
 while :
 do
-  echo -e " \033[1;32mボタン調査を開始します\033[0;39m"
-  read -p " 宜しいですか? [y/n]: " num
-  case "${num}" in
-    "y" ) ls /etc/rc.button/
-          
-
+echo -e " \033[1;32mボタン設定とインストール（WPSボタン用）のスクリプトをダウンロードします\033[0;39m"
+read -p " 宜しいですか? [y/n]: " num
+case "${num}" in
+    "y" ) wget --no-check-certificate -O /etc/config-software/button.sh https://raw.githubusercontent.com/site-u2023/config-software/main/button.sh
+          sed -i -e "s/BUTTON='wps'/BUTTON='${str_BUTTON}'/g" /etc/config-software/button.sh
+          sh /etc/config-software/button.sh
           break ;;
     "n" ) break ;;
   esac
@@ -34,9 +54,9 @@ done
 function _func_button_REMOVE {
 while :
 do
-  echo -e " \033[1;32mボタン設定をリムーブし以前の設定に復元します\033[0;39m"
+  echo -e " \033[1;31mボタン設定をリムーブし以前の設定に復元します\033[0;39m"
   read -p " 宜しいですか? [y/n]: " num
-  case "${num}" in
+   case "${num}" in
     "y" ) opkg remove kmod-button-hotplug
           cp /etc/config/system.button.bak /etc/config/system
           rm /etc/config/system.button.bak
@@ -47,7 +67,6 @@ do
   esac
 done
 }
-
 
 OPENWRT_RELEAS=`grep -o '[0-9]*' /etc/openwrt_version`
 if [ "${OPENWRT_RELEAS:0:2}" = "23" ] || [ "${OPENWRT_RELEAS:0:2}" = "21" ] || [ "${OPENWRT_RELEAS:0:2}" = "22" ]; then
@@ -61,7 +80,7 @@ while :
 do
   echo -e " \033[1;37mbutton-config ----------------------------------------\033[0;39m"
   echo -e " \033[1;34m[w]\033[0;39m": ボタン設定とインストール（WPSボタン用）
-  echo -e " \033[1;32m[c]\033[0;39m": ボタン調査
+  echo -e " \033[1;32m[c]\033[0;39m": ボタン調査後ボタン設定とインストール
   echo -e " \033[1;31m[r]\033[0;39m": ボタン設定をリムーブし以前の設定に復元
   echo -e " \033[1;37m[q]\033[0;39m": 終了
   echo -e " \033[1;37m------------------------------------------------------\033[0;39m"
