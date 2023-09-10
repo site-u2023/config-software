e60b768b7b14801b2e31fabe7d0348b33ac13f64
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-10 21:10:52 +0900

    Update package-config.sh

--- package-config.sh
+++ package-config.sh
@@ -5,7 +5,7 @@
 function _func_full_INST
 while :
 do
-  echo -e " \033[1;3自動フルインストールのスクリプトをダウンロードします\033[0;39m"
+  echo -e " \033[1;34自動フルインストールのスクリプトをダウンロードします\033[0;39m"
   read -p " 宜しいですか? [y/n]: " num
   case "${num}" in
     "y" ) wget --no-check-certificate -O /etc/config-software/package-auto.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-auto.sh
@@ -18,11 +18,25 @@ done
 function _func_choice_INST
 while :
 do
-  echo -e " \033[1;32m選択インストールのスクリプトをダウンロードします\033[0;39m"
+  echo -e " \033[1;33m選択インストールのスクリプトをダウンロードします\033[0;39m"
   read -p " 宜しいですか? [y/n]: " num
   case "${num}" in
     "y" ) wget --no-check-certificate -O /etc/config-software/package-manual.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-manual.sh
           sh /etc/config-software/package-manual.sh
+          break ;;
+    "n" ) break ;;
+  esac
+done
+
+function _func_choice_INST
+while :
+do
+  echo -e " \033[1;31mフラッシュ後にインストールしたパッケージ確認のスクリプトをダウンロードし実行します\033[0;39m"
+  read -p " 宜しいですか? [y/n]: " num
+  case "${num}" in
+    "y" ) wget --no-check-certificate -O /etc/config-software/install-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/install-config.sh
+          sh /etc/config-software/install-config.sh
+          read -p " 何かキーを押して下さい"
           break ;;
     "n" ) break ;;
   esac
@@ -41,13 +55,15 @@ do
   echo -e " \033[1;37mインストールは失敗する事があります\033[0;39m"
   echo -e " \033[1;37mpackage-config ---------------------------------------\033[0;39m"
   echo -e " \033[1;34m[f]\033[0;39m": 自動フルインストール（推奨パッケージオールイン）
-  echo -e " \033[1;32m[c]\033[0;39m": 選択インストール
+  echo -e " \033[1;33m[c]\033[0;39m": 選択インストール
+  echo -e " \033[1;31m[a]\033[0;39m": フラッシュ後にインストールしたパッケージ確認
   echo -e " \033[1;37m[q]\033[0;39m": 終了
   echo -e " \033[1;37m------------------------------------------------------\033[0;39m"
   read -p " キーを選択してください [f/c or q]: " num 
   case "${num}" in
     "f" ) _func_full_INST ;;
     "c" ) _func_choice_INST ;;
+    "a" ) _func_choice_INST ;;
     "q" ) exit ;;
   esac
 done
