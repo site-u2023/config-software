38c98bd38d9a21120e54357aecac6aa3c448ddef
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-12 10:21:01 +0900

    Update ad-dns-blocking-config.sh

--- ad-dns-blocking-config.sh
+++ ad-dns-blocking-config.sh
@@ -68,10 +68,10 @@ if [ "${OPENWRT_RELEAS:0:2}" = "23" ] || [ "${OPENWRT_RELEAS:0:2}" = "21" ] || [
  exit
 fi
 }
-  echo -e " \033[1;33mAdGuardとAdBlockは排他利用\033[0;39m"
-  echo -e " \033[1;33mAdGuardとDNS over HTTPS（DoH）は排他利用\033[0;39m"
-  echo -e " \033[1;33mAdGuardとDNS over TLS（DoT）は排他利用\033[0;39m"
-  echo -e " \033[1;33mDNS over HTTPS（DoH）とDNS over TLS（DoT）は排他利用\033[0;39m"
+  echo -e " \033[7;33mAdGuardとAdBlockは排他利用\033[0;39m"
+  echo -e " \033[7;33mAdGuardとDNS over HTTPS（DoH）は排他利用\033[0;39m"
+  echo -e " \033[7;33mAdGuardとDNS over TLS（DoT）は排他利用\033[0;39m"
+  echo -e " \033[7;33mDNS over HTTPS（DoH）とDNS over TLS（DoT）は排他利用\033[0;39m"
   echo -e " \033[1;37mad dns blocking config ------------------------------\033[0;39m"
   echo -e " \033[1;34m[g]\033[0;39m": AdGuardの設定とインストール（カスタムフィルターアドイン）
   echo -e " \033[1;32m[b]\033[0;39m": AdBlockdのインストールと設定（カスタムフィルターアドイン）
