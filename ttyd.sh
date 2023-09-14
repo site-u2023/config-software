0e11932acad1fee4f4e17e98845837fc000ebd66
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-09-14 14:08:04 +0900

    Create ttyd.sh

---
+++ ttyd.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+cat <<"EOF" >> /usr/bin/confsoft
+#!/bin/sh
+mkdir -p /etc/config-software
+wget --no-check-certificate -O /etc/config-software/openwrt-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/openwrt-config.sh
+sh /etc/config-software/openwrt-config.sh
+EOF
+chmod +x /usr/bin/confsoft
+
+UPDATE="/tmp/opkg-lists/openwrt_telephony.sig"
+if [ ! -e ${UPDATE} ]; then
+opkg update
+fi
+opkg install ttyd
+opkg install luci-app-ttyd
+opkg install luci-i18n-ttyd-ja
+
+uci set ttyd.@ttyd[0]=ttyd
+uci set ttyd.@ttyd[0].interface='@lan'
+uci set ttyd.@ttyd[0].command='/bin/login -f root'
+uci set ttyd.@ttyd[0].ipv6='1'
+uci set ttyd.@ttyd[0].debug='7'
+uci set ttyd.@ttyd[0].url_arg='1'
+uci add_list ttyd.@ttyd[0].client_option='fontSize=25'
+uci add_list ttyd.@ttyd[0].client_option='theme={"background": "black"}'
+uci add_list ttyd.@ttyd[0].client_option='titleFixed=TTYD'
+uci set ttyd.ttyd=ttyd
+uci set ttyd.ttyd.port='8888'
+uci set ttyd.ttyd.interface='@lan'
+uci set ttyd.ttyd.ipv6='1'
+uci set ttyd.ttyd.debug='7'
+uci set ttyd.ttyd.command='confsoft'
+uci set ttyd.ttyd.url_arg='1'
+uci add_list ttyd.ttyd.client_option='fontSize=25'
+uci add_list ttyd.ttyd.client_option='theme={"background": "blue"}'
+uci add_list ttyd.ttyd.client_option='titleFixed=CONFIG-SOFTWARE'
+
+uci commit ttyd
+service ttyd enable
+service ttyd restart
