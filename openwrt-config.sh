7dcf1587ff5d38d723df603a71e4bec6921a2647
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-11-17 11:41:01 +0900

    Update openwrt-config.sh

--- openwrt-config.sh
+++ openwrt-config.sh
@@ -4,8 +4,8 @@
 function _func_SYSTEM {
 while :
 do
-  echo -e " \033[1;34mシステム初期設定のスクリプトをダウンロードします\033[0;39m"
-  read -p " 宜しいですか? [y/n]: " num
+  echo -e " \033[1;34mDownload scripts for initial system\033[0;39m"
+  read -p " All right? [y/n]: " num
   case "${num}" in
     "y" ) wget --no-check-certificate -O /etc/config-software/system-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/system-config.sh
           sh /etc/config-software/system-config.sh
@@ -18,8 +18,8 @@ done
 function _func_INTERNET {
 while :
 do
-  echo -e " \033[1;33mインターネット接続設定のスクリプトをダウンロードします\033[0;39m"
-  read -p " 宜しいですか? [y/n]: " num
+  echo -e " \033[1;33mDownload scripts for Internet connection\033[0;39m"
+  read -p " All right? [y/n]: " num
   case "${num}" in
     "y" ) wget --no-check-certificate -O /etc/config-software/internet-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/internet-config.sh
           sh /etc/config-software/internet-config.sh
@@ -32,8 +32,8 @@ done
 function _func_PACKAGE {
 while :
 do
-  echo -e " \033[1;32m推奨パッケージインストール（自動or選択）のスクリプトをダウンロードします\033[0;39m"
-  read -p " 宜しいですか? [y/n]: " num
+  echo -e " \033[1;32mDownload script for package setup (automatic or selective)\033[0;39m"
+  read -p " All right? [y/n]: " num
   case "${num}" in
     "y" ) wget --no-check-certificate -O /etc/config-software/package-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-config.sh
           sh /etc/config-software/package-config.sh
@@ -46,8 +46,8 @@ done
 function _func_ACCESSPOINT {
 while :
 do
-  echo -e " \033[1;35mアクセスポイント設定のスクリプトをダウンロードします\033[0;39m"
-  read -p " 宜しいですか? [y/n]: " num
+  echo -e " \033[1;35mDownload scripts for access point\033[0;39m"
+  read -p " All right? [y/n]: " num
   case "${num}" in
     "y" ) wget --no-check-certificate -O /etc/config-software/dumb-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/dumb-config.sh
           sh /etc/config-software/dumb-config.sh
@@ -60,8 +60,8 @@ done
 function _func_AD_DNS {
 while :
 do
-  echo -e " \033[1;31mDNS暗号化・広告ブロッカーインストールのスクリプトをダウンロードします\033[0;39m"
-  read -p " 宜しいですか? [y/n]: " num
+  echo -e " \033[1;31mDownload scripts for ad blocker and DNS encryption\033[0;39m"
+  read -p " All right? [y/n]: " num
   case "${num}" in
     "y" ) wget --no-check-certificate -O /etc/config-software/ad-dns-blocking-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/ad-dns-blocking-config.sh
           sh /etc/config-software/ad-dns-blocking-config.sh
@@ -74,8 +74,8 @@ done
 function _func_ETC {
 while :
 do
-  echo -e " \033[1;36m他のスクリプトをダウンロードします\033[0;39m"
-  read -p " 宜しいですか? [y/n]: " num
+  echo -e " \033[1;36mDownload other scripts\033[0;39m"
+  read -p " All right? [y/n]: " num
   case "${num}" in
     "y" ) wget --no-check-certificate -O /etc/config-software//etc-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/etc-config.sh
           sh /etc/config-software/etc-config.sh
@@ -88,14 +88,14 @@ done
 function _func_QUIT {
 while :
 do
-  echo -e " \033[7;40mこのスクリプト及び関連スクリプトを削除します\033[0;39m"
-  echo -e " \033[7;40m復元用ファイルは削除されません\033[0;39m"
+  echo -e " \033[7;40mDelete this and related scripts\033[0;39m"
+  echo -e " \033[7;40mRestoration files will not be deleted\033[0;39m"
   read -p " 削除しますか? [y/n or r]: " num
    case "${num}" in
     "y" ) rm -rf /etc/config-software
-          echo -e " \033[7;40mこのスクリプト及び関連スクリプトを削除して終了しました\033[0;39m"
+          echo -e " \033[7;40mThis script and related scripts have been deleted and terminated\033[0;39m"
           exit ;;
-    "n" ) echo -e " \033[7;40m終了しました\033[0;39m"
+    "n" ) echo -e " \033[7;40mClosed\033[0;39m"
           exit ;;
     "r" ) break ;;
    esac 
@@ -115,47 +115,45 @@ sleep 1
 
 OPENWRT_RELEAS=`cat /etc/banner | grep OpenWrt | awk '{ print $2 }' | cut -c 1-2`
 if [ "${OPENWRT_RELEAS}" = "23" ] || [ "${OPENWRT_RELEAS}" = "22" ] || [ "${OPENWRT_RELEAS}" = "21" ]; then
- echo -e " \033[1;37mバージョンチェック: OK\033[0;39m"
+ echo -e " \033[1;37mVersion Checking: OK\033[0;39m"
  else
- read -p " バージョンが違うため終了します"
+ read -p " Exit due to different versions"
  exit
 fi
 
   AVAILABLE_MEMORY=`free | fgrep 'Mem:' | awk '{ print $4 }'`
   AVAILABLE_FLASH=`df | fgrep 'overlayfs:/overlay' | awk '{ print $4 }'`
-  echo -e " \033[7;33m不具合はこちらまで  https://qiita.com/site_u\033[0;39m" 
   echo -e " \033[1;37m-----------------------------------------------------\033[0;39m"
   echo -e " \033[1;37mOpenWrt-CONFIG\033[0;39m"
-  echo -e " \033[1;37m最終更新: 202311130254"
-  echo -e " \033[1;37m利用可能メモリー容量: ${AVAILABLE_MEMORY}KB\033[0;39m"
-  echo -e " \033[1;37m利用可能フラッシュ容量: ${AVAILABLE_FLASH}KB\033[0;39m"
-  echo -e " \033[1;37mスクリプト設置ディレクトリ: /etc/config-software/\033[0;39m" 
+  echo -e " \033[1;37mLast update: 202311171006"
+  echo -e " \033[1;37mAvailable Memory Space: ${AVAILABLE_MEMORY}KB\033[0;39m"
+  echo -e " \033[1;37mAvailable Flash Space: ${AVAILABLE_FLASH}KB\033[0;39m"
+  echo -e " \033[1;37mDirectory to install scripts: /etc/config-software/\033[0;39m" 
   echo -e " \033[1;37mhttps://github.com/site-u2023/config-software\033[0;39m" 
-  echo -e " \033[7;31m当スクリプトご利用に対し、一切の責任は負いかねます\033[0;39m"
+  echo -e " \033[1;37mhttps://qiita.com/site_u\033[0;39m" 
+  echo -e " \033[7;31mWe take no responsibility for the use of this script.\033[0;39m"
   echo -e " \033[1;37m-----------------------------------------------------\033[0;39m"
-  read -p "  何かキーを押して下さい"
+  read -p "  Press any key"
   echo -e 
   
 while :
 do
-  echo -e " \033[1;37mJapanese notation\033[0;39m"
-  echo -e " \033[7;33m推奨設定順序: システム > インターネット > パッケージ\033[0;39m"  
   echo -e " \033[1;37mopenwrt-config --------------------------------------\033[0;39m"
-  echo -e " \033[1;34m[s]\033[0;39m": システム初期設定（ホストネーム・パスワード等）
-  echo -e " \033[1;33m[i]\033[0;39m": インターネット（MAP-e・DS-LITE・PPPoE）接続設定
-  echo -e " \033[1;32m[p]\033[0;39m": 推奨パッケージインストール（USB自動判別）
-  echo -e " \033[1;35m[a]\033[0;39m": アクセスポイント（Dumb / ブリッジ）設定
-  echo -e " \033[1;31m[d]\033[0;39m": 広告ブロッカー・DNS暗号化インストール
-  echo -e " \033[1;36m[e]\033[0;39m": 他：ボタン（スイッチ） / IPERF3 / WiFi位置情報サービス停止設定
-  echo -e " \033[7;40m[q]\033[0;39m": 終了
+  echo -e " \033[1;33m[i]\033[0;39m": Internet connection setup (MAP-e, DS-LITE, PPPoE)
+  echo -e " \033[1;34m[s]\033[0;39m": Initial system setup (host name, password, etc.)
+  echo -e " \033[1;32m[p]\033[0;39m": Install recommended packages (automatic device identification)
+  echo -e " \033[1;31m[b]\033[0;39m": Install ad blocker and DNS encryption
+  echo -e " \033[1;35m[a]\033[0;39m": Access point connection setup (Dumb / Bridge)
+  echo -e " \033[1;36m[e]\033[0;39m": Other: BUTTON/IPERF3/SAMBA4/LBS
+  echo -e " \033[7;40m[q]\033[0;39m": Quit
   echo -e " \033[1;37m-----------------------------------------------------\033[0;39m"
-  read -p " キーを選択してください [s/i/p/a/d/e or q]: " num
+  read -p " Press any key [i/s/p/b/a/e or q]: " num
   case "${num}" in
-    "s" ) _func_SYSTEM ;;
     "i" ) _func_INTERNET ;;
+    "s" ) _func_SYSTEM ;;
     "p" ) _func_PACKAGE ;;
+    "b" ) _func_AD_DNS ;;
     "a" ) _func_ACCESSPOINT ;;
-    "d" ) _func_AD_DNS ;;
     "e" ) _func_ETC ;;
     "q" ) _func_QUIT ;;
   esac
