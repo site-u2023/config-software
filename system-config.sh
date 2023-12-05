99ae6c3b2075dbd9b3260549717b500f71019aba
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-12-05 17:21:23 +0900

    Update system-config.sh

--- system-config.sh
+++ system-config.sh
@@ -218,7 +218,7 @@ function _func_DEVICE_SET {
   wget --no-check-certificate -O /etc/config-software/system.sh https://raw.githubusercontent.com/site-u2023/config-software/main/system.sh
   sed -i -e "s/HOSTNAME='openwrt'/HOSTNAME=${input_str_SYSTEM_HOSTNAME}/g" /etc/config-software/system.sh
   sed -i -e "s/ROOT_PASSWD/${input_str_ROOT_PASSWD}/g" /etc/config-software/system.sh
-  sed -i -e "s/country='JP'/country=${input_str_Languages}/g" /etc/config-software/system.sh
+  sed -i -e "s/LANGUAGE='COUNTRY'/LANGUAGE=${input_str_Languages}/g" /etc/config-software/system.sh
   sed -i -e "s/WIFI_SSID_A='SSID_A'/WIFI_SSID_A=${input_str_WIFI_SSID_A}/g" /etc/config-software/system.sh
   sed -i -e "s/WIFI_PASSWORD_A='password'/WIFI_PASSWORD_A=${input_str_WIFI_PASSWD_A}/g" /etc/config-software/system.sh
   sed -i -e "s/WIFI_SSID_B='SSID_B'/WIFI_SSID_B=${input_str_WIFI_SSID_B}/g" /etc/config-software/system.sh
