1220ab9b1e007c180afa6815f2792145db365ab0
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-12-06 15:46:24 +0900

    Update package-manual.sh

--- package-manual.sh
+++ package-manual.sh
@@ -2,7 +2,22 @@
 # OpenWrt >= 21.02:
 
 
-function _func_Languages {
+function _func_Languages_check {
+while :
+do
+  echo -e " \033[1;37mLanguage --------------------------------------------\033[0;39m"
+  echo -e " \033[1;34m[e]: English (English)\033[0;39m"
+  echo -e " \033[1;32m[g]: Other than English\033[0;39m"
+  echo -e " \033[1;37m-----------------------------------------------------\033[0;39m"
+  read -p " Press any key [e or g]: " num
+  case "${num}" in
+    "e" ) wget --no-check-certificate -O /etc/config-software/package-manual-e.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-manual-e.sh
+          sh /etc/config-software/package-manual-e.sh ;;
+    "g" ) _func_Languages_code ;;
+  esac
+ done 
+
+function _func_Languages_code {
 while :
 do
   echo -e " \033[1;37mar: العربية (Arabic)\033[0;39m"
