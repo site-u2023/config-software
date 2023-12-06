df74b223b9cf220c100056babc6e22b57d7dfaf4
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-12-06 16:19:51 +0900

    Update package-manual.sh

--- package-manual.sh
+++ package-manual.sh
@@ -3,12 +3,14 @@ do
   echo -e " \033[1;37mLanguage --------------------------------------------\033[0;39m"
   echo -e " \033[1;34m[e]: English (English)\033[0;39m"
   echo -e " \033[1;32m[g]: Other than English\033[0;39m"
+  echo -e " \033[1;32m[g]: Quit\033[0;39m"
   echo -e " \033[1;37m-----------------------------------------------------\033[0;39m"
-  read -p " Press any key [e or g]: " num
+  read -p " Press any key [e/g or q]: " num
   case "${num}" in
     "e" ) wget --no-check-certificate -O /etc/config-software/package-manual-e.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-manual-e.sh
           sh /etc/config-software/package-manual-e.sh ;;
     "g" ) wget --no-check-certificate -O /etc/config-software/package-manual-g.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-manual-g.sh
           sh /etc/config-software/package-manual-g.sh ;;
+    "q" ) exit ;;
   esac
  done 
