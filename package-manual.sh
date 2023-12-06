ac51e4876cbb1018b4eec143861d967579337667
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-12-06 16:11:12 +0900

    Create package-manual.sh

---
+++ package-manual.sh
@@ -0,0 +1,15 @@
+function _func_Languages_check {
+while :
+do
+  echo -e " \033[1;37mLanguage --------------------------------------------\033[0;39m"
+  echo -e " \033[1;34m[e]: English (English)\033[0;39m"
+  echo -e " \033[1;32m[g]: Other than English\033[0;39m"
+  echo -e " \033[1;37m-----------------------------------------------------\033[0;39m"
+  read -p " Press any key [e or g]: " num
+  case "${num}" in
+    "e" ) wget --no-check-certificate -O /etc/config-software/package-manual-e.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-manual-e.sh
+          sh /etc/config-software/package-manual-e.sh ;;
+    "g" ) wget --no-check-certificate -O /etc/config-software/package-manual-g.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-manual-g.sh
+          sh /etc/config-software/package-manual-g.sh ;;
+  esac
+ done 
