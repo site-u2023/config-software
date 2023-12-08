697469c1cb050dc3d79209a0d38a029a611e17bb
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-12-08 18:39:22 +0900

    Update etc-config.sh

--- etc-config.sh
+++ etc-config.sh
@@ -33,8 +33,8 @@ do
           network_get_ipaddr NET_ADDR "${NET_IF}"
           sed -i -e "s/192.168.1.1/${NET_ADDR}/g" /etc/init.d/iperf3
           echo -e " \033[1;32mInstallation is complete\033[0;39m"
-          echo -e " \033[1;36menable: service iperf3 enable\033[0;39m"
-          echo -e " \033[1;36mdisable: service iperf3 disable\033[0;39m"
+          echo -e " \033[1;36mEnable: service iperf3 enable\033[0;39m"
+          echo -e " \033[1;36mDisable: service iperf3 disable\033[0;39m"
           echo -e " \033[1;36mStart: service iperf3 start\033[0;39m"
           echo -e " \033[1;36mStop: service iperf3 stop\033[0;39m"
           read -p " Start service [y/n]: " snum
@@ -54,9 +54,6 @@ function _func_WiFi_location_service {
 while :
 do
   echo -e " \033[1;32mExecute Location Based Service (LBS) stop setting\033[0;39m"
-  echo -e " \033[1;36m`uci show wireless.default_radio0.ssid`\033[0;39m"
-  echo -e " \033[1;36m`uci show wireless.default_radio1.ssid`\033[0;39m"
-  echo -e " \033[1;36m`uci show wireless.default_radio2.ssid`\033[0;39m"
   echo -e " \033[1;32mAdd _optout_nomap to the SSID name\033[0;39m"
   read -p " Press any key [y/n]: " num
   case "${num}" in
