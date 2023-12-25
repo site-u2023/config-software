e680b2279b24c8ccfbfc560b41dfa77017bc7632
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-12-25 15:37:38 +0900

    Update openwrt-config.sh

--- openwrt-config.sh
+++ openwrt-config.sh
@@ -144,8 +144,8 @@ fi
   
 while :
 do
-  echo -e " \033[1;33m Multilingual support (Partially unchanged)\033[0;39m"
-  echo -e " \033[1;33m Internet connection setup requires device to be connected directly to ONU\033[0;39m"
+  echo -e " \033[1;33mMultilingual support (Partially unchanged)\033[0;39m"
+  echo -e " \033[1;33mInternet connection setup requires device to be connected directly to ONU\033[0;39m"
   echo -e " \033[1;37mopenwrt-config --------------------------------------\033[0;39m"
   echo -e " \033[1;33m[i]: Internet connection setup in Japan (MAP-e, DS-LITE, PPPoE)\033[0;39m"
   echo -e " \033[1;34m[s]: Initial system setup (host name,password,etc.)\033[0;39m"
