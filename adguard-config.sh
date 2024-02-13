a1c94959f143a1f2fbbbfdbb37f74c766e4770d1
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-02-14 01:44:11 +0900

    Update adguard-config.sh

--- adguard-config.sh
+++ adguard-config.sh
@@ -197,10 +197,11 @@ echo -e " \033[1;32m管理用ウェブインターフェイス: http://${NET_ADD
 read -p " 何かキーを押してデバイスを再起動して下さい"
 reboot
 exit
-else
+ else
 echo -e " \033[1;32m管理用ウェブインターフェイスの設定が完了しました\033[0;39m"
 echo -e " \033[1;32m管理用ウェブインターフェイス: http://${NET_ADDR}:${input_str_PORT}\033[0;39m"
 read -p " 何かキーを押してデバイスを再起動して下さい"
+reboot
 exit
 fi
 }
