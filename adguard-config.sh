8e8ad06692b71c04f7599b9b847376348375280d
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-02-13 23:43:23 +0900

    Update adguard-config.sh

--- adguard-config.sh
+++ adguard-config.sh
@@ -36,7 +36,7 @@ echo -e " \033[1;37m利用可能フラッシュ容量: /overlayfs:/overlayがあ
 echo -e " \033[1;37m利用可能フラッシュ容量: ${AVAILABLE_FLASH}KB\033[0;39m"
   fi
 echo -e " \033[1;37mインストール容量: ${ADGUARD_SIZE}KB\033[0;39m"
-  if [ "${AVAILABLE_FLASH}" -gt ${ADGUARD_SIZE} ]; then
+  if [ "${AVAILABLE_FLASH}" -gt ${ADGUARD_SIZE} ] || [ -z "${AVAILABLE_FLASH}" ]; then
    echo -e " \033[1;37mメモリー容量推奨値: 51200KB\033[0;39m"
    echo -e " \033[1;37mフラッシュ容量推奨値: 102400KB\033[0;39m"
    echo -e " \033[1;37mインストール可能です\033[0;39m"
