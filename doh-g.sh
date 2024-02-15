eaf4c0f83f9825c2d9358a3bd2a87197b9b46b0c
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-02-15 22:41:34 +0900

    Update doh-g.sh

--- doh-g.sh
+++ doh-g.sh
@@ -40,7 +40,7 @@ do
   echo -e " \033[1;37mLanguage packs will not install without code entry (Standard: English)\033[0;39m"
   echo -e " \033[1;37mExample: ja\033[0;39m"
   read -p " Language: " input_str_Languages
-  read -p " Press any key  [y/n or r]: " num
+  read -p " Please select key [y/n or r]: " num
   echo -e " \033[1;32mLanguages: $input_str_Languages\033[0;39m"
   case "${num}" in
     "y" ) _func_HTTPS ;;
@@ -57,7 +57,7 @@ do
   echo -e " \033[1;37mInstall: https-dns-proxy\033[0;39m"
   echo -e " \033[1;37mInstall: luci-app-https-dns-proxy\033[0;39m"
   echo -e " \033[1;37mInstall: luci-i18n-https-dns-proxy-ja\033[0;39m" 
-  read -p " Press any key [y/n]: " num
+  read -p " Please select key [y/n]: " num
   case "${num}" in
     "y" ) _func_HTTPS_SET ;;
     "n" ) exit ;;
@@ -95,7 +95,7 @@ do
   echo -e " \033[1;37mDNS over HTTPS（DoH）settings to previous settings\033[0;39m"
   echo -e " \033[1;37mRemove: luci-app-https-dns-proxy\033[0;39m"
   echo -e " \033[1;37mRemove: https-dns-proxy\033[0;39m"
-  read -p " Press any key [y/n or r]: " num
+  read -p " Please select key [y/n or r]: " num
 
   case "${num}" in
     "y" ) _func_HTTPS_Restoration ;;
@@ -137,7 +137,7 @@ do
   echo -e " \033[1;31m[b]: DNS over HTTPS（DoH）settings to previous settings\033[0;39m"
   echo -e " \033[1;33m[r]: Quit\033[0;39m"    
   echo -e " \033[1;34m------------------------------------------------------\033[0;39m"
-  read -p " Press any key [e/b or r]: " num
+  read -p " Please select key [e/b or r]: " num
   case "${num}" in
     "e" ) _func_Languages ;;
     "b" ) _func_HTTPS_Before ;;
