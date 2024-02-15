6770c8f9c57b0e98ae00f3be33ec5f123dae1728
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-02-15 22:18:20 +0900

    Update package-config.sh

--- package-config.sh
+++ package-config.sh
@@ -6,7 +6,7 @@ function _func_full_INST {
 while :
 do
   echo -e " \033[1;34Download automatic full installation scripts\033[0;39m"
-  read -p " Press any key [y/n]: " num
+  read -p " Please select key [y/n]: " num
   case "${num}" in
     "y" ) wget --no-check-certificate -O /etc/config-software/package-auto.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-auto.sh
           sh /etc/config-software/package-auto.sh
@@ -20,7 +20,7 @@ function _func_choice_INST {
 while :
 do
   echo -e " \033[1;33mDownload selective installation scripts\033[0;39m"
-  read -p " Press any key [y/n]: " num
+  read -p " Please select key [y/n]: " num
   case "${num}" in
     "y" ) wget --no-check-certificate -O /etc/config-software/package-manual.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-manual.sh
           sh /etc/config-software/package-manual.sh
@@ -33,8 +33,8 @@ done
 function _func_after_INST {
 while :
 do
-  echo -e " \033[1;31mDownload and run the script to confirm the installed package after flashing\033[0;39m"
-  read -p " Press any key [y/n]: " num
+  echo -e " \033[1;31mDownload and run script to confirm installed package after flashing\033[0;39m"
+  read -p " Please select key [y/n]: " num
   case "${num}" in
     "y" ) wget --no-check-certificate -O /etc/config-software/install-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/install-config.sh
           sh /etc/config-software/install-config.sh
@@ -62,7 +62,7 @@ do
   echo -e " \033[1;31m[a]: Confirmation of packages installed after flashing\033[0;39m"
   echo -e " \033[1;37m[q]: Quit\033[0;39m"
   echo -e " \033[1;32m------------------------------------------------------\033[0;39m"
-  read -p " Press any key [f/s/a or q]: " num 
+  read -p " Please select key [f/s/a or q]: " num 
   case "${num}" in
     "f" ) _func_full_INST ;;
     "s" ) _func_choice_INST ;;
