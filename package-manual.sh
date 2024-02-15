16fa6ec01fc6d0884d54212ddd128162e1673128
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-02-15 22:16:30 +0900

    Update package-manual.sh

--- package-manual.sh
+++ package-manual.sh
@@ -1,11 +1,11 @@
 while :
 do
-  echo -e " \033[1;33mPackage manual language -----------------------------\033[0;39m"
+  echo -e " \033[1;33mManual Package language -----------------------------\033[0;39m"
   echo -e " \033[1;36m[e]: English\033[0;39m"
   echo -e " \033[1;31m[o]: Other\033[0;39m"
   echo -e " \033[1;33m[q]: Quit\033[0;39m"
   echo -e " \033[1;33m-----------------------------------------------------\033[0;39m"
-  read -p " Press any key [e/o or q]: " num
+  read -p " Please select key [e/o or q]: " num
   case "${num}" in
     "e" ) wget --no-check-certificate -O /etc/config-software/package-manual-e.sh https://raw.githubusercontent.com/site-u2023/config-software/main/package-manual-e.sh
           sh /etc/config-software/package-manual-e.sh ;;
