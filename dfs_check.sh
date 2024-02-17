cd87c055f742ab71a043d32bf887ce9082b62666
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-02-18 02:40:51 +0900

    Create dfs_check.sh

---
+++ dfs_check.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+CHECK=$(iw dev phy0-ap0 info | awk '/channel/{print $2}')
+if [ -z ${CHECK} ]; then
+        printf "Wifi restart because of DFS bug!\n"
+        wifi down radio0
+        sleep 2
+        wifi up radio0
+else
+        if [ $(uci get wireless.radio1.channel) -ne ${CHECK} ]; then
+                printf "Wifi restart because of channel!\n"
+                wifi down radio0
+                sleep 2
+                wifi up radio0
+       fi
+fi
+
