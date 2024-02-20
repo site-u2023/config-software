15b2f38f19725f37856b02837f8f6ed6dab0ab01
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-02-20 12:26:25 +0900

    Update package-manual-g.sh

--- package-manual-g.sh
+++ package-manual-g.sh
@@ -1151,7 +1151,7 @@ fi
 
 # Internet detector
 if [ -z "$DETECTER" ]; then
-INTERNET_DETECTOR_V=`cat /etc/config-software/pacage_list | awk '{print substr($0,index($0,"current/internet-detector") ,39)}'`
+INTERNET_DETECTOR=`cat /etc/config-software/pacage_list | awk '{print substr($0,index($0,"current/internet-detector_1") ,39)}'`
 wget --no-check-certificate -O /tmp/internet-detector_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/${INTERNET_DETECTOR_V}
 opkg install /tmp/internet-detector_all.ipk
 rm /tmp/internet-detector_all.ipk
