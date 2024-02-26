46bdf3a40c879108f65609552d2b9124df3fb866
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-02-26 10:14:06 +0900

    Update package-manual-e.sh

--- package-manual-e.sh
+++ package-manual-e.sh
@@ -3,10 +3,7 @@
 
 
 function _func_listinstalled_Before {
-UPDATE="/tmp/opkg-lists/openwrt_telephony"
-if [ ! -e ${UPDATE} ]; then
 opkg update
-fi
 opkg install wget-ssl	
 mkdir -p /etc/config-software/list-installed
 echo 0 > /etc/config-software/list-installed/Flash
