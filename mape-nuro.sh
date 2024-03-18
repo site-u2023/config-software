07af276c0314fd50981f9c42b9249f2f3b453a80
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-03-18 20:52:12 +0900

    Update mape-nuro.sh

--- mape-nuro.sh
+++ mape-nuro.sh
@@ -3,7 +3,6 @@
 . /lib/functions/network.sh
 network_flush_cache
 network_find_wan6 NET_IF6
-network_get_ipaddr6 NET_ADDR6 "${NET_IF6}"
 NURO_V6=`echo ${NET_ADDR6} | cut -d: -f1-3`
 
 function _func_NURO {
