cb4b2d790304750276b158f86d4bff15fd1d1f09
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-04-12 00:59:09 +0900

    Update system.sh

--- system.sh
+++ system.sh
@@ -76,64 +76,119 @@ uci commit dhcp
 # /etc/init.d/dnsmasq restart
 # /etc/init.d/odhcpd restart
 
-# Wi-Fi A channels
 COUNTRY='Country_Code'
+# Wi-Fi A channels
+RADIO_A='radio'
 WIFI_SSID_A='SSID_A'
 WIFI_PASSWORD_A='password'
-uci set wireless.radio0.country=${COUNTRY}
-uci set wireless.radio0.txpower='10'
-# uci set wireless.radio0.noscan='1'
-uci set wireless.radio0.frag='2347'
-uci set wireless.radio0.rts='2346'
-uci set wireless.radio0.cell_density='1'
-uci set wireless.default_radio0.encryption='sae-mixed'
-uci set wireless.default_radio0.ssid=${WIFI_SSID_A}
-uci set wireless.default_radio0.key=${WIFI_PASSWORD_A}
-uci set wireless.default_radio0.multicast_to_unicast='1'
-uci set wireless.default_radio0.isolate='1'
-uci set wireless.default_radio0.multicast_to_unicast_all='1'
-uci set wireless.default_radio0.macaddr='random' # Random Mac Address
-# uci set wireless.default_radio0.wpa_disable_eapol_key_retries='1'
+uci set wireless.${RADIO_A}.country=${COUNTRY}
+uci set wireless.${RADIO_A}.txpower='10'
+# uci set wireless.${RADIO_A}.noscan='1'
+uci set wireless.${RADIO_A}.frag='2347'
+uci set wireless.${RADIO_A}.rts='2346'
+uci set wireless.${RADIO_A}.cell_density='1'
+uci set wireless.default_${RADIO_A}.encryption='sae-mixed'
+uci set wireless.default_${RADIO_A}.ssid=${WIFI_SSID_A}
+uci set wireless.default_${RADIO_A}.key=${WIFI_PASSWORD_A}
+uci set wireless.default_${RADIO_A}.multicast_to_unicast='1'
+uci set wireless.default_${RADIO_A}.isolate='1'
+uci set wireless.default_${RADIO_A}.multicast_to_unicast_all='1'
+uci set wireless.default_${RADIO_A}.macaddr='random' # Random Mac Address
+# uci set wireless.default_${RADIO_A}.wpa_disable_eapol_key_retries='1'
+# Wi-Fi AA channels
+RADIO_AA='radio'
+WIFI_SSID_AA='SSID_AA'
+WIFI_PASSWORD_AA='password'
+uci set wireless.${RADIO_AA}.country=${COUNTRY}
+uci set wireless.${RADIO_AA}.txpower='10'
+# uci set wireless.${RADIO_AA}.noscan='1'
+uci set wireless.${RADIO_AA}.frag='2347'
+uci set wireless.${RADIO_AA}.rts='2346'
+uci set wireless.${RADIO_AA}.cell_density='1'
+uci set wireless.default_${RADIO_AA}.encryption='sae-mixed'
+uci set wireless.default_${RADIO_AA}.ssid=${WIFI_SSID_AA}
+uci set wireless.default_${RADIO_AA}.key=${WIFI_PASSWORD_AA}
+uci set wireless.default_${RADIO_AA}.multicast_to_unicast='1'
+uci set wireless.default_${RADIO_AA}.isolate='1'
+uci set wireless.default_${RADIO_AA}.multicast_to_unicast_all='1'
+uci set wireless.default_${RADIO_AA}.macaddr='random' # Random Mac Address
+# uci set wireless.default_${RADIO_AA}.wpa_disable_eapol_key_retries='1'
 # Wi-Fi B channel
-COUNTRY='Country_Code'
+RADIO_B='radio'
 WIFI_SSID_B='SSID_B'
 WIFI_PASSWORD_B='password'
-uci set wireless.radio1.country=${COUNTRY}
-uci set wireless.radio1.txpower='10'
-# uci set wireless.radio1.noscan='1'
-uci set wireless.radio1.frag='2347'
-uci set wireless.radio1.rts='2346'
-uci set wireless.radio1.cell_density='1'
-uci set wireless.default_radio1.encryption='sae-mixed'
-uci set wireless.default_radio1.ssid=${WIFI_SSID_B}
-uci set wireless.default_radio1.key=${WIFI_PASSWORD_B}
-uci set wireless.default_radio1.multicast_to_unicast='1'
-uci set wireless.default_radio1.isolate='1'
-uci set wireless.default_radio1.multicast_to_unicast_all='1'
-uci set wireless.default_radio1.macaddr='random' # Random Mac Address
-# uci set wireless.default_radio1.wpa_disable_eapol_key_retries='1'
+uci set wireless.${RADIO_B}.country=${COUNTRY}
+uci set wireless.${RADIO_B}.txpower='10'
+# uci set wireless.${RADIO_B}.noscan='1'
+uci set wireless.${RADIO_B}.frag='2347'
+uci set wireless.${RADIO_B}.rts='2346'
+uci set wireless.${RADIO_B}.cell_density='1'
+uci set wireless.default_${RADIO_B}.encryption='sae-mixed'
+uci set wireless.default_${RADIO_B}.ssid=${WIFI_SSID_B}
+uci set wireless.default_${RADIO_B}.key=${WIFI_PASSWORD_B}
+uci set wireless.default_${RADIO_B}.multicast_to_unicast='1'
+uci set wireless.default_${RADIO_B}.isolate='1'
+uci set wireless.default_${RADIO_B}.multicast_to_unicast_all='1'
+uci set wireless.default_${RADIO_B}.macaddr='random' # Random Mac Address
+# uci set wireless.default_${RADIO_B}.wpa_disable_eapol_key_retries='1'
+# Wi-Fi BB channel
+RADIO_BB='radio'
+WIFI_SSID_BB='SSID_BB'
+WIFI_PASSWORD_BB='password'
+uci set wireless.${RADIO_BB}.country=${COUNTRY}
+uci set wireless.${RADIO_BB}.txpower='10'
+# uci set wireless.${RADIO_BB}.noscan='1'
+uci set wireless.${RADIO_BB}.frag='2347'
+uci set wireless.${RADIO_BB}.rts='2346'
+uci set wireless.${RADIO_BB}.cell_density='1'
+uci set wireless.default_${RADIO_BB}.encryption='sae-mixed'
+uci set wireless.default_${RADIO_BB}.ssid=${WIFI_SSID_BB}
+uci set wireless.default_${RADIO_BB}.key=${WIFI_PASSWORD_BB}
+uci set wireless.default_${RADIO_BB}.multicast_to_unicast='1'
+uci set wireless.default_${RADIO_BB}.isolate='1'
+uci set wireless.default_${RADIO_BB}.multicast_to_unicast_all='1'
+uci set wireless.default_${RADIO_BB}.macaddr='random' # Random Mac Address
+# uci set wireless.default_${RADIO_BB}.wpa_disable_eapol_key_retries='1'
 # Wi-Fi C channel
-COUNTRY='Country_Code'
+RADIO_C='radio'
 WIFI_SSID_C='SSID_C'
 WIFI_PASSWORD_C='password'
-uci set wireless.radio2.country=${COUNTRY}
-uci set wireless.radio2.txpower='10'
-# uci set wireless.radio2.noscan='1'
-uci set wireless.radio2.frag='2347'
-uci set wireless.radio2.rts='2346'
-uci set wireless.radio2.cell_density='1'
-uci set wireless.default_radio2.encryption='sae-mixed'
-uci set wireless.default_radio2.ssid=${WIFI_SSID_C}
-uci set wireless.default_radio2.key=${WIFI_PASSWORD_C}
-uci set wireless.default_radio2.multicast_to_unicast='1'
-uci set wireless.default_radio2.isolate='1'
-uci set wireless.default_radio2.multicast_to_unicast_all='1'
-uci set wireless.default_radio2.macaddr='random' # Random Mac Address
-# uci set wireless.default_radio1.wpa_disable_eapol_key_retries='1'
+uci set wireless.${RADIO_C}.country=${COUNTRY}
+uci set wireless.${RADIO_C}.txpower='10'
+# uci set wireless.${RADIO_C}.noscan='1'
+uci set wireless.${RADIO_C}.frag='2347'
+uci set wireless.${RADIO_C}.rts='2346'
+uci set wireless.${RADIO_C}.cell_density='1'
+uci set wireless.default_${RADIO_C}.encryption='sae-mixed'
+uci set wireless.default_${RADIO_C}.ssid=${WIFI_SSID_C}
+uci set wireless.default_${RADIO_C}.key=${WIFI_PASSWORD_C}
+uci set wireless.default_${RADIO_C}.multicast_to_unicast='1'
+uci set wireless.default_${RADIO_C}.isolate='1'
+uci set wireless.default_${RADIO_C}.multicast_to_unicast_all='1'
+uci set wireless.default_${RADIO_C}.macaddr='random' # Random Mac Address
+# uci set wireless.default_${RADIO_C}.wpa_disable_eapol_key_retries='1'
+# Wi-Fi CC channel
+RADIO_CC='radio'
+WIFI_SSID_CC='SSID_CC'
+WIFI_PASSWORD_CC='password'
+uci set wireless.${RADIO_CC}.country=${COUNTRY}
+uci set wireless.${RADIO_CC}.txpower='10'
+# uci set wireless.${RADIO_CC}.noscan='1'
+uci set wireless.${RADIO_CC}.frag='2347'
+uci set wireless.${RADIO_CC}.rts='2346'
+uci set wireless.${RADIO_CC}.cell_density='1'
+uci set wireless.default_${RADIO_CC}.encryption='sae-mixed'
+uci set wireless.default_${RADIO_CC}.ssid=${WIFI_SSID_CC}
+uci set wireless.default_${RADIO_CC}.key=${WIFI_PASSWORD_CC}
+uci set wireless.default_${RADIO_CC}.multicast_to_unicast='1'
+uci set wireless.default_${RADIO_CC}.isolate='1'
+uci set wireless.default_${RADIO_CC}.multicast_to_unicast_all='1'
+uci set wireless.default_${RADIO_CC}.macaddr='random' # Random Mac Address
+# uci set wireless.default_${RADIO_C}.wpa_disable_eapol_key_retries='1'
 # set
-uci delete wireless.radio0.disabled
-uci delete wireless.radio1.disabled
-uci delete wireless.radio2.disabled
+uci delete wireless.${RADIO_A}.disabled
+uci delete wireless.${RADIO_B}.disabled
+uci delete wireless.${RADIO_C}.disabled
 uci commit wireless
 # /etc/init.d/network restart
 
