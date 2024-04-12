9a4de4bf8ce0d6f40e3a822f74cf9a92728e2c19
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-04-12 13:57:59 +0900

    Update system-config.sh

--- system-config.sh
+++ system-config.sh
@@ -451,7 +451,6 @@ function _func_DEVICE_SET {
   wget --no-check-certificate -O /etc/config-software/dfs-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/dfs-config.sh
   sh /etc/config-software/dfs-config.sh 2> /dev/null
   service dfs_check enable
-  service dfs_check start
   fi
   sh /etc/config-software/system.sh 2> /dev/null
   if [ "$GUEST" = "on" ]; then
@@ -461,8 +460,7 @@ function _func_DEVICE_SET {
   opkg install at
   wget --no-check-certificate -O /etc/config-software/guest.sh https://raw.githubusercontent.com/site-u2023/config-software/main/guest.sh
   bash /etc/config-software/guest.sh 2> /dev/null
-  service guest_wifi disable
-  service guest_wifi stop
+  service guest_wifi enable
   fi
   echo -e " \033[1;32mLuCi: http://${input_str_SYSTEM_HOSTNAME}.lan\033[0;39m"
   if [ "$GUEST" = "on" ]; then
