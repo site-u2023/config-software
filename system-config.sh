c77141cf2b80a3599ebba37229121db2d2c55430
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-04-12 13:59:31 +0900

    Update system-config.sh

--- system-config.sh
+++ system-config.sh
@@ -451,6 +451,7 @@ function _func_DEVICE_SET {
   wget --no-check-certificate -O /etc/config-software/dfs-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/dfs-config.sh
   sh /etc/config-software/dfs-config.sh 2> /dev/null
   service dfs_check enable
+  service dfs_check start
   fi
   sh /etc/config-software/system.sh 2> /dev/null
   if [ "$GUEST" = "on" ]; then
