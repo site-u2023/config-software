68dfa6ed81bb28c890ed06c0bbdfbadec4ef9e68
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-04-16 18:38:51 +0900

    Update dfs-check-new-config.sh

--- dfs-check-new-config.sh
+++ dfs-check-new-config.sh
@@ -1,22 +1,32 @@
 #! /bin/sh
+    
+# オートチャンネル帯域設定
 RADIO=`uci show wireless | grep "band='5g'" | cut -d'.' -f2 | awk '{ print $1 }'`
 uci set wireless.${RADIO}.channels='36 40 44 48'
 uci commit wireless
 wifi reload ${RADIO}
+
+# スクリプト
 mkdir -p /etc/config-software/
 cat << "EOF" > /etc/config-software/dfs_check_new.sh
 #! /bin/sh
 
+# DFS用チャンネル及び帯域幅設定
 DFS_CHANNEL="auto"
 DFS_BAND="40"
 
+# インターバル時間抽出
 read INTERVAL < /tmp/config-software/interval.txt
 SCHEDULE=`expr $((${INTERVAL} * 121))`
+
+# 5Gデバイス確認
 RADIO=`uci show wireless | grep "band='5g'" | cut -d'.' -f2 | awk '{ print $1 }'`
 CHS=`echo ${RADIO} | wc -w`
 if [ ${CHS} = 2 ];then
     RADIO=`echo ${RADIO}| awk '{print $2}'`
 fi
+
+# 5Gチャンネル及び帯域幅初期値抽出
 echo `uci get wireless.${RADIO}.channel` >> /tmp/config-software/channel.txt
 echo `uci get wireless.${RADIO}.htmode` >> /tmp/config-software/htmode.txt
 read CHANNEL < /tmp/config-software/channel.txt
@@ -24,26 +34,32 @@ read HTMODE < /tmp/config-software/htmode.txt
 MODE=`echo ${HTMODE} | grep -o "[A-Z]*"`
 BAND=`echo ${HTMODE} | grep -o "[0-9]*"`
 CH=`echo ${RADIO} | grep -o "[0-9]*"`
+
+# 5Gチャンネル状態確認
 DEV=`iw dev | awk '/Interface/{print $2}' | grep ${CH}`
 IW_CHANNEL=$(iw dev ${DEV} info | awk '/channel/{print $2}')
 if [ -z ${IW_CHANNEL} ]; then
-    logger "DFS Check NEW:bug"
+    logger "DFS Check NEW: bug"
     wifi reload ${RADIO}
 else
     if [ $(uci get wireless.${RADIO}.channel) -ne ${CHANNEL} ] || [ $(uci get wireless.${RADIO}.htmode) != ${HTMODE} ]; then
-        logger "DFS Check NEW:verification"
+        logger "DFS Check NEW: verification"
         wifi reload ${RADIO}
     fi
 fi
+
+# DFSログ時間抽出
 DATE=`date +%s`
 DATE_DISABLE=`exec logread | grep "DFS->DISABLED" | tail -n 1 | awk '{ print $4 }'`
 DATE_ENABLE=`exec logread | grep "DFS->ENABLED" | tail -n 1 | awk '{ print $4 }'`
+
+# DFS状態確認及び対応
 if [ -n "${DATE_DISABLE}" ] && [ -z "${DATE_ENABLE}" ]; then
     if [ $(uci get wireless.${RADIO}.channel) -ne ${DFS_CHANNEL} ] && [ $(uci get wireless.${RADIO}.htmode) != ${MODE}${DFS_BAND} ]; then
         DATE_DISABLEDS=`date +%s -d "${DATE_DISABLE}"`
         TIME=`expr $((${DATE} - ${DATE_DISABLEDS}))`
     	if [ ${TIME} -lt ${SCHEDULE} ]; then
-            logger "DFS Check NEW:enable"
+            logger "DFS Check NEW: enable"
             uci set wireless.${RADIO}.channel=${DFS_CHANNEL}
             uci set wireless.${RADIO}.htmode=${MODE}${DFS_BAND}
             uci commit wireless
@@ -51,7 +67,7 @@ if [ -n "${DATE_DISABLE}" ] && [ -z "${DATE_ENABLE}" ]; then
             exit 0
         fi
     else
-        logger "DFS Check NEW:use"
+        logger "DFS Check NEW: use"
         exit 0
 	fi
 fi
@@ -61,7 +77,7 @@ if [ -n "${DATE_DISABLE}" ] && [ -n "${DATE_ENABLE}" ]; then
             DATE_ENABLEDS=`date +%s -d "${DATE_ENABLE}"`
             TIME=`expr $((${DATE} - ${DATE_ENABLEDS}))`
         	if [ ${TIME} -lt ${SCHEDULE} ]; then
-                logger "DFS Check NEW:disable"
+                logger "DFS Check NEW: disable"
                 uci set wireless.${RADIO}.channel=${CHANNEL}
                 uci set wireless.${RADIO}.htmode=${MODE}${BAND}
                 uci commit wireless
@@ -69,7 +85,7 @@ if [ -n "${DATE_DISABLE}" ] && [ -n "${DATE_ENABLE}" ]; then
                 exit 0
             fi
         else
-            logger "DFS Check NEW:normal"
+            logger "DFS Check NEW: normal"
             exit 0
         fi
     else
@@ -78,7 +94,7 @@ if [ -n "${DATE_DISABLE}" ] && [ -n "${DATE_ENABLE}" ]; then
                 DATE_DISABLEDS=`date +%s -d "${DATE_DISABLE}"`
                 TIME=`expr $((${DATE} - ${DATE_DISABLEDS}))`
             	if [ ${TIME} -lt ${SCHEDULE} ]; then
-                    logger "DFS Check NEW:enable"
+                    logger "DFS Check NEW: enable"
                     uci set wireless.${RADIO}.channel=${DFS_CHANNEL}
                     uci set wireless.${RADIO}.htmode=${MODE}${DFS_BAND}
                     uci commit wireless
@@ -86,7 +102,7 @@ if [ -n "${DATE_DISABLE}" ] && [ -n "${DATE_ENABLE}" ]; then
                     exit 0
                 fi
             else
-                logger "DFS Check NEW:use"
+                logger "DFS Check NEW: use"
                 exit 0  
             fi
         fi
@@ -97,7 +113,7 @@ if [ -n "${DATE_ENABLE}" ]; then
         DATE_ENABLEDS=`date +%s -d "${DATE_ENABLE}"`
         TIME=`expr $((${DATE} - ${DATE_ENABLEDS}))`
         if [ ${TIME} -lt ${SCHEDULE} ]; then
-            logger "DFS Check NEW:disable"
+            logger "DFS Check NEW: disable"
             uci set wireless.${RADIO}.channel=${CHANNEL}
             uci set wireless.${RADIO}.htmode=${MODE}${BAND}
             uci commit wireless
@@ -105,7 +121,7 @@ if [ -n "${DATE_ENABLE}" ]; then
             exit 0
         fi
     else
-        logger "DFS Check NEW:normal"
+        logger "DFS Check NEW: normal"
         exit 0
     fi
 fi
@@ -113,16 +129,18 @@ EOF
 chmod +x /etc/config-software/dfs_check_new.sh
 
 
+# サービス
 cat << "EOF" > /etc/init.d/dfs_check_new
 #!/bin/sh /etc/rc.common
 
+# インターバル時間設定
 INTERVAL="5" # DFS check interval (minutes)
 
 START=99
 STOP=01
     
 start() {
-    logger "DFS Check NEW:start"
+    logger "DFS Check NEW: start"
     rm -rf /tmp/config-software
     sed -i "/dfs_check_new.sh/d" /etc/crontabs/root
     echo "*/${INTERVAL} * * * * sh /etc/config-software/dfs_check_new.sh # DFS Check NEW enable" >> /etc/crontabs/root
@@ -135,7 +153,7 @@ restart() {
     exit 0
 }
 stop() {
-    logger "DFS Check NEW:stop"
+    logger "DFS Check NEW: stop"
     sed -i "/dfs_check_new.sh/d" /etc/crontabs/root
     /etc/init.d/cron restart
     rm -rf /tmp/config-software
@@ -145,6 +163,7 @@ EOF
 chmod +x /etc/init.d/dfs_check_new
 
 
+# 動作確認スクリプト
 cat <<"EOF" >> /usr/bin/dfslog
 #!/bin/sh
 echo DFS Check NOW -----------------------
@@ -156,5 +175,4 @@ exec logread | grep "DFS->ENABLED"  | tail -n 1 | awk '{ print $1,$2,$3,$4,$5,$1
 echo --------------------------------------
 EOF
 chmod +x /usr/bin/dfslog
-
 
