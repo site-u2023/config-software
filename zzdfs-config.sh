9df43cd9a92b9a1270ec667efce0c315f0f79ff7
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-04-25 18:08:45 +0900

    Update zzdfs-config.sh

--- zzdfs-config.sh
+++ zzdfs-config.sh
@@ -84,7 +84,7 @@ if [ $? = 0 ]; then
             echo "*/${INTERVAL} * * * * sh /etc/config-software/zzdfs.sh # ZZDFS" >> /etc/crontabs/root
             /etc/init.d/cron restart
         else
-            logger "ZZDFS: Abnormality"
+            logger "ZZDFS: No_radio"
             sed -i "/zzdfs.sh/d" /etc/crontabs/root
             /etc/init.d/cron restart
             wifi reload ${RADIO}
