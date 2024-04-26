9e2d569b6f1821ad25a79e884994d80646c7d5bb
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-04-26 14:09:13 +0900

    Update zzdfs-config.sh

--- zzdfs-config.sh
+++ zzdfs-config.sh
@@ -11,6 +11,14 @@ START=99
 STOP=01
      
 start() {
+    uci get system.@system[0].log_file 2&>1
+    if [ $? = 1 ]; then
+        uci set system.@system[0].log_file='/var/log/syslog'
+        uci set system.@system[0].log_remote='0'
+        uci commit system
+        service log restart
+        service system restart
+    fi
     sleep 1m 15s
     mkdir -p /tmp/config-software/
     RADIO=`uci show wireless | grep "band='5g'" | cut -d'.' -f2 | awk '{ print $1 }'`
@@ -19,9 +27,8 @@ start() {
         RADIO=`echo ${RADIO}| awk '{print $2}'`
     fi
     CH=`echo ${RADIO} | grep -o "[0-9]*"`
-    DEV=`iw dev | awk '/Interface/{print $2}' | grep ${CH}`
+    DEV=$(iw dev | awk '/Interface/{print $2}' | grep ${CH} | tee /tmp/config-software/dev)
     echo ${INTERVAL} > /tmp/config-software/interval
-    echo ${DEV} > /tmp/config-software/dev
     echo ${RADIO} > /tmp/config-software/radio
     echo ${FB_BAND} > /tmp/config-software/fb_band
     echo ${FB_CHANNEL} > /tmp/config-software/fb_channel
@@ -53,29 +60,26 @@ read DEV < /tmp/config-software/dev
 iwinfo ${DEV} info 2>&1 | grep -q 'No such wireless device'
 if [ $? = 0 ]; then
     read RADIO < /tmp/config-software/radio
-    uci get wireless.${RADIO}.disabled 2>&1
-    if [ $? = 1 ]; then
+    WIFI=`uci get wireless.${RADIO}.disabled`
+    if [ "${WIFI}" != 1 ]; then 
         exec logread 2>&1 | grep "DFS->DISABLED"
         if [ $? = 0 ]; then
-            logger "ZZDFS: ZZDFS_ON"
+            logger "ZZDFS: ZZDFS_On"
+            sed -i -e "s/DFS->DISABLED/DFS->disabled/g" /var/log/syslog
             sed -i "/zzdfs.sh/d" /etc/crontabs/root
             /etc/init.d/cron restart
             read INTERVAL < /tmp/config-software/interval
             read FB_CHANNEL < /tmp/config-software/fb_channel
             read FB_BAND < /tmp/config-software/fb_band
-            CHANNEL=$(uci get wireless.${RADIO}.channel)
-            HTMODE=$(uci get wireless.${RADIO}.htmode)
+            CHANNEL=$(uci get wireless.${RADIO}.channel | grep ${CH} | tee /tmp/config-software/channel)
+            HTMODE=$(uci get wireless.${RADIO}.htmode | grep ${CH} | tee /tmp/config-software/htmode)
             MODE=`echo ${HTMODE} | grep -o "[A-Z]*"`
-            echo ${CHANNEL} > /tmp/config-software/channel
-            echo ${HTMODE} > /tmp/config-software/htmode
             uci set wireless.${RADIO}.channel=${FB_CHANNEL}
             uci set wireless.${RADIO}.htmode=${MODE}${FB_BAND}
             uci commit wireless
             wifi reload ${RADIO}
             sleep 30m
-            logger "ZZDFS: ZZDFS_OFF"
-            read CHANNEL < /tmp/config-software/channel
-            read HTMODE < /tmp/config-software/htmode
+            logger "ZZDFS: ZZDFS_Off"
             uci set wireless.${RADIO}.channel=${CHANNEL}
             uci set wireless.${RADIO}.htmode=${HTMODE}
             uci commit wireless
@@ -84,7 +88,7 @@ if [ $? = 0 ]; then
             echo "*/${INTERVAL} * * * * sh /etc/config-software/zzdfs.sh # ZZDFS" >> /etc/crontabs/root
             /etc/init.d/cron restart
         else
-            logger "ZZDFS: No_radio"
+            logger "ZZDFS: No_Radio"
             sed -i "/zzdfs.sh/d" /etc/crontabs/root
             /etc/init.d/cron restart
             sleep 1m 10s
@@ -95,6 +99,7 @@ if [ $? = 0 ]; then
         fi
     else
         logger "ZZDFS: ${RADIO}_Disable"
+        fi
     fi    
 fi
 EOF
@@ -148,7 +153,7 @@ echo -e "\033[1;36mLOG ------------------------------------\033[0;39m"
 echo -e "\033[1;37mLOG:\033[0;39m"
 exec logread | grep "ZZDFS" | awk '{ print $1,$2,$3,$4,$5,$9 }' | tail -n 10
 echo -e "\033[1;37mDISABLED:\033[0;39m"
-exec logread | grep "DFS->DISABLED" | awk '{ print $1,$2,$3,$4,$5,$11 }' | tail -n 1
+exec logread | grep "DFS->disabled" | awk '{ print $1,$2,$3,$4,$5,$11 }' | tail -n 1
 echo -e "\033[1;37mENABLED:\033[0;39m"
 exec logread | grep "DFS->ENABLED" | awk '{ print $1,$2,$3,$4,$5,$11 }' | tail -n 1
 echo -e "\033[1;36mINFORMATION ----------------------------\033[0;39m"
@@ -163,6 +168,12 @@ echo -e "\033[1;37mFALLBACK Channel/ Htmode: ${FB_CHANNEL}Ch/ ${MODE}${FB_BAND}\
 read RADIO < /tmp/config-software/radio
 CHANNEL=$(uci get wireless.${RADIO}.channel)
 HTMODE=$(uci get wireless.${RADIO}.htmode)
+WIFI=`uci get wireless.${RADIO}.disabled` 2>/dev/null
+if [ "${WIFI}" != 1 ]; then
+    echo -e "\033[1;37mWi-Fi 5G ${RADIO} ENABLE\033[0;39m"   
+else
+    echo -e "\033[1;37mWi-Fi 5G ${RADIO} DISABLE\033[0;39m"
+fi
 echo -e "\033[1;37mWi-Fi 5G Channel/ Htmode: ${CHANNEL}Ch/ ${HTMODE}\033[0;39m"
 echo -e "\033[1;36m----------------------------------------\033[0;39m"
 EOF
