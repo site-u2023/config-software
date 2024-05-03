428fbf65d6e1c9eace8a97fd0e855c14b30de169
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-05-03 14:41:01 +0900

    Update ttyd.sh

--- ttyd.sh
+++ ttyd.sh
@@ -20,7 +20,7 @@ uci set ttyd.@ttyd[0].debug='7'
 uci set ttyd.@ttyd[0].url_arg='1'
 uci add_list ttyd.@ttyd[0].client_option='fontSize=25'
 uci add_list ttyd.@ttyd[0].client_option='theme={"background": "black"}'
-uci add_list ttyd.@ttyd[0].client_option='titleFixed=UCI'
+uci add_list ttyd.@ttyd[0].client_option='titleFixed=ttyd'
 uci set ttyd.ttyd=ttyd
 uci set ttyd.ttyd.port='8888'
 uci set ttyd.ttyd.interface='@lan'
@@ -30,7 +30,7 @@ uci set ttyd.ttyd.command='confsoft'
 uci set ttyd.ttyd.url_arg='1'
 uci add_list ttyd.ttyd.client_option='fontSize=25'
 uci add_list ttyd.ttyd.client_option='theme={"background": "blue"}'
-uci add_list ttyd.ttyd.client_option='titleFixed=CONFIG-SOFTWARE'
+uci add_list ttyd.ttyd.client_option='titleFixed=config-software'
 
 uci commit ttyd
 service ttyd restart
