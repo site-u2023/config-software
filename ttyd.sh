7b43061605d8f83e683fb50fd5971706409fd65c
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-05-03 22:08:03 +0900

    Update ttyd.sh

--- ttyd.sh
+++ ttyd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cat <<"EOF" >> /usr/bin/confsoft
+cat <<"EOF" > /usr/bin/confsoft
 #!/bin/sh
 mkdir -p /etc/config-software
 wget --no-check-certificate -O /etc/config-software/openwrt-config.sh https://raw.githubusercontent.com/site-u2023/config-software/main/openwrt-config.sh
