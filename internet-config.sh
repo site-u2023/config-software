b2bede99f2146997bffb04624b2bfac4a09ce642
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-12-07 04:00:37 +0900

    Update internet-config.sh

--- internet-config.sh
+++ internet-config.sh
@@ -111,7 +111,6 @@ exit
 
 function _func_MAP_e_Restoration {
 cp /etc/config/network.map-e.old /etc/config/network
-rm /etc/config/network.map-e.old
 cp /etc/config/dhcp.map-e.old /etc/config/dhcp
 cp /etc/config/firewall.map-e.old /etc/config/firewall
 cp /lib/netifd/proto/map.sh.old /lib/netifd/proto/map.sh
