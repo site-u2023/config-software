73e3a861a59040b808a92dcb2cc98ccfd599d124
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-12-06 17:37:51 +0900

    Update adguard_19.sh

--- adguard_19.sh
+++ adguard_19.sh
@@ -1,4 +1,12 @@
 #! /bin/sh
+OPENWRT_RELEAS=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
+if [[ "${OPENWRT_RELEAS}" = "20" || "${OPENWRT_RELEAS}" = "19" ]]; then
+   echo -e " The version of this device is \033[1;33m$OPENWRT_RELEAS\033[0;39m"
+   echo -e " Version Check: \033[1;36mOK\033[0;39m"
+ else
+   read -p " Exit due to different versions"
+ exit
+fi
 if [ -e /etc/init.d/AdGuardHome ]; then
 read -p  "AdGuardHome is installed on this device." 
 exit
@@ -15,6 +23,10 @@ wget  --no-check-certificate -O /tmp/config-software/AdGuardHome_list https://gi
 AdGuardHome_list=`cat /tmp/config-software/AdGuardHome_list`
 latest_ver=`echo $AdGuardHome_list | awk '{print substr($0,index($0,"AdGuard Home v") ,30)}' | awk '{ sub("</span>.*$",""); print $0; }' | grep -o -E "(v[0-9]+\.){1}[0-9]+(\.[0-9]+)?" | head -n1`
 # Releases
+opkg update
+opkg install libc
+opkg install ca-bundle
+mkdir -p /etc/AdGuardHome
 wget --no-check-certificate -O /etc/AdGuardHome/AdGuardHome_linux_armv7.tar.gz https://github.com/AdguardTeam/AdGuardHome/releases/download/${latest_ver}/AdGuardHome_linux_armv7.tar.gz
 tar -xzvf /etc/AdGuardHome/AdGuardHome_linux_armv7.tar.gz -C /etc/
 rm -rf /etc/AdGuardHome/AdGuardHome_linux_armv7.tar.gz
