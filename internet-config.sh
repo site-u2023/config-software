0150f5346de5c582c29a53a5c56d9add0d2c781b
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-12-07 16:16:35 +0900

    Update internet-config.sh

--- internet-config.sh
+++ internet-config.sh
@@ -90,7 +90,15 @@ done
 
 function _func_MAP_e_Nichiban_map_SET {
 cp /lib/netifd/proto/map.sh /lib/netifd/proto/map.sh.old
-wget --no-check-certificate -O /lib/netifd/proto/map.sh https://raw.githubusercontent.com/site-u2023/map-e/main/map.sh.new
+# Version-specific settings
+OPENWRT_RELEAS=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
+if [[ "${OPENWRT_RELEAS}" = "23" || "${OPENWRT_RELEAS}" = "22" || "${OPENWRT_RELEAS}" = "21" ]]; then
+wget -6 --no-check-certificate -O /lib/netifd/proto/map.sh https://raw.githubusercontent.com/site-u2023/map-e/main/map.sh.new
+fi
+OPENWRT_RELEAS=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
+if [[ "${OPENWRT_RELEAS}" = "20" || "${OPENWRT_RELEAS}" = "19" ]]; then
+wget -6 --no-check-certificate -O /lib/netifd/proto/map.sh https://raw.githubusercontent.com/site-u2023/map-e/main/map19.sh.new
+fi
 read -p " 何かキーを押してデバイスを再起動してください"
 reboot
 }
