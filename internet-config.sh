5090344a113255e06a8ce477e2265284c50895e9
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-12-07 18:08:38 +0900

    Update internet-config.sh

--- internet-config.sh
+++ internet-config.sh
@@ -57,6 +57,8 @@ OPENWRT_RELEAS=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | c
 if [[ "${OPENWRT_RELEAS}" = "23" || "${OPENWRT_RELEAS}" = "22" || "${OPENWRT_RELEAS}" = "21" ]]; then
   wget -6 --no-check-certificate -O /lib/netifd/proto/map.sh https://raw.githubusercontent.com/site-u2023/map-e/main/map.sh.new
 elif [[ "${OPENWRT_RELEAS}" = "20" || "${OPENWRT_RELEAS}" = "19" ]]; then
+  cp /etc/firewall.user /etc/firewall.user.old
+  wget -6 --no-check-certificate -O /etc/firewall.user https://raw.githubusercontent.com/site-u2023/map-e/main/firewall.user.new
   wget -6 --no-check-certificate -O /lib/netifd/proto/map.sh https://raw.githubusercontent.com/site-u2023/map-e/main/map19.sh.new
   opkg update
   opkg install iptables-mod-ipopt
@@ -83,6 +85,22 @@ do
 done
 }
 
+function _func_MAP_e_Restoration {
+cp /etc/config/network.map-e.old /etc/config/network
+cp /etc/config/dhcp.map-e.old /etc/config/dhcp
+cp /etc/config/firewall.map-e.old /etc/config/firewall
+cp /lib/netifd/proto/map.sh.old /lib/netifd/proto/map.sh
+cp /etc/firewall.user.old /etc/firewall.user
+rm /etc/config/network.map-e.old
+rm /etc/config/dhcp.map-e.old
+rm /etc/config/firewall.map-e.old
+rm /lib/netifd/proto/map.sh.old
+rm /etc/firewall.user.old
+read -p " 何かキーを押してデバイスを再起動してください"
+reboot
+exit
+}
+
 function _func_MAP_e_Nichiban_map {
 while :
 do
@@ -99,13 +117,18 @@ done
 function _func_MAP_e_Nichiban_map_SET {
 cp /lib/netifd/proto/map.sh /lib/netifd/proto/map.sh.old
 # Version-specific settings
-OPENWRT_RELEAS=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
-if [[ "${OPENWRT_RELEAS}" = "23" || "${OPENWRT_RELEAS}" = "22" || "${OPENWRT_RELEAS}" = "21" ]]; then
-wget -6 --no-check-certificate -O /lib/netifd/proto/map.sh https://raw.githubusercontent.com/site-u2023/map-e/main/map.sh.new
+OPENWRT_RELEAS=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-5)
+if [[ "${OPENWRT_RELEAS}" = "23.05" ]]; then
+read -p "不要です"
+exit
 fi
 OPENWRT_RELEAS=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
-if [[ "${OPENWRT_RELEAS}" = "20" || "${OPENWRT_RELEAS}" = "19" ]]; then
-wget -6 --no-check-certificate -O /lib/netifd/proto/map.sh https://raw.githubusercontent.com/site-u2023/map-e/main/map19.sh.new
+if [[ "${OPENWRT_RELEAS}" = "23" || "${OPENWRT_RELEAS}" = "22" || "${OPENWRT_RELEAS}" = "21" ]]; then
+  wget -6 --no-check-certificate -O /lib/netifd/proto/map.sh https://raw.githubusercontent.com/site-u2023/map-e/main/map.sh.new
+elif [[ "${OPENWRT_RELEAS}" = "20" || "${OPENWRT_RELEAS}" = "19" ]]; then
+  cp /etc/firewall.user /etc/firewall.user.old
+  wget -6 --no-check-certificate -O /etc/firewall.user https://raw.githubusercontent.com/site-u2023/map-e/main/firewall.user.new
+  wget -6 --no-check-certificate -O /lib/netifd/proto/map.sh https://raw.githubusercontent.com/site-u2023/map-e/main/map19.sh.new
 fi
 read -p " 何かキーを押してデバイスを再起動してください"
 reboot
@@ -126,21 +149,9 @@ done
 
 function _func_MAP_e_Nichiban_map_Restoration {
 cp /lib/netifd/proto/map.sh.old /lib/netifd/proto/map.sh
+cp /etc/firewall.user.old /etc/firewall.user
 rm /lib/netifd/proto/map.sh.old
-read -p " 何かキーを押してデバイスを再起動してください"
-reboot
-exit
-}
-
-function _func_MAP_e_Restoration {
-cp /etc/config/network.map-e.old /etc/config/network
-cp /etc/config/dhcp.map-e.old /etc/config/dhcp
-cp /etc/config/firewall.map-e.old /etc/config/firewall
-cp /lib/netifd/proto/map.sh.old /lib/netifd/proto/map.sh
-rm /etc/config/network.map-e.old
-rm /etc/config/dhcp.map-e.old
-rm /etc/config/firewall.map-e.old
-rm /lib/netifd/proto/map.sh.old
+rm /etc/firewall.user.old
 read -p " 何かキーを押してデバイスを再起動してください"
 reboot
 exit
