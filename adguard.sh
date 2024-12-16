cf3cd89ec8cd4f54902e97662a860fe3ec4def4b
site_u <140032047+site-u2023@users.noreply.github.com>
2024-12-16 09:39:53 +0900

    Update adguard.sh

--- adguard.sh
+++ adguard.sh
@@ -4,30 +4,15 @@ cp /etc/config/network /etc/config/network.adguard.bak
 cp /etc/config/dhcp /etc/config/dhcp.adguard.bak
 cp /etc/config/firewall /etc/config/firewall.adguard.bak
 
-function _func_Package {
-opkg install adguardhome
-_func_Install
-}
+OPENWRT_RELEAS=$(grep 'DISTRIB_RELEASE' /etc/openwrt_release | cut -d"'" -f2 | cut -c 1-2)
+if [[ "${OPENWRT_RELEAS}" = "24" || "${OPENWRT_RELEAS}" = "23" || "${OPENWRT_RELEAS}" = "22" || "${OPENWRT_RELEAS}" = "21" ]]; then
+  opkg update
+  opkg install adguardhome
+elif [[ "${OPENWRT_RELEAS}" = "SN" ]]; then
+  apk update
+  apk add adguardhome
+fi
 
-function _func_Native {
-#　Check version
-mkdir -p /tmp/config-software
-wget --no-check-certificate -O /tmp/config-software/AdGuardHome_list https://github.com/AdguardTeam/AdGuardHome
-AdGuardHome_list=`cat /tmp/config-software/AdGuardHome_list`
-latest_ver=`echo $AdGuardHome_list | awk '{print substr($0,index($0,"AdGuard Home v") ,30)}' | awk '{ sub("</span>.*$",""); print $0; }' | grep -o -E "(v[0-9]+\.){1}[0-9]+(\.[0-9]+)?" | head -n1`
-echo -e " \033[1;33m Install Version : ${latest_ver}\033[0;39m"
-read -p "  Press any key"
-# Install
-opkg install ca-bundle
-mkdir -p /etc/AdGuardHome
-wget --no-check-certificate -O /etc/AdGuardHome/AdGuardHome_linux_armv7.tar.gz https://github.com/AdguardTeam/AdGuardHome/releases/download/${latest_ver}/AdGuardHome_linux_armv7.tar.gz
-tar -xzvf /etc/AdGuardHome/AdGuardHome_linux_armv7.tar.gz -C /etc/
-rm -rf /etc/AdGuardHome/AdGuardHome_linux_armv7.tar.gz
-/etc/AdGuardHome/AdGuardHome -s install
-_func_Install 
-}
-
-function _func_Install {
 service adguardhome enable
 service adguardhome start
 NET_ADDR=$(/sbin/ip -o -4 addr list br-lan | awk 'NR==1{ split($4, ip_addr, "/"); print ip_addr[1] }')
@@ -65,20 +50,3 @@ uci set firewall.adguardhome_dns_53.family="any"
 uci commit firewall
 /etc/init.d/firewall restart
 exit
-}
-
-UPDATE="/tmp/opkg-lists/openwrt_telephony"
-if [ ! -e ${UPDATE} ]; then
-opkg update
-fi
-
-while :
-do
-  echo -e " \033[1;34m[p]: Package install\033[0;39m"
-  echo -e " \033[1;33m[n]: Native install\033[0;39m"
-  read -p " Which do you install [p or n]: " num
-  case "${num}" in
-    "p" ) _func_Package ;;
-    "n" ) _func_Native ;;
-  esac
- done
