de16d2d2f038d748d7c9cba75b52030d53f1322a
site_u <140032047+site-u2023@users.noreply.github.com>
2024-12-18 13:59:13 +0900

    Update velop_wrt_pro7_adguard.sh

--- velop_wrt_pro7_adguard.sh
+++ velop_wrt_pro7_adguard.sh
@@ -2,6 +2,7 @@
 
 opkg install ca-bundle
 mkdir -p /etc/AdGuardHome
+latest_ver=`echo $AdGuardHome_list | awk '{print substr($0,index($0,"AdGuard Home v") ,30)}' | awk '{ sub("</span>.*$",""); print $0; }' | grep -o -E "(v[0-9]+\.){1}[0-9]+(\.[0-9]+)?" | head -n1`
 wget --no-check-certificate -O /etc/AdGuardHome/AdGuardHome_linux_armv7.tar.gz https://github.com/AdguardTeam/AdGuardHome/releases/download/${latest_ver}/AdGuardHome_linux_armv7.tar.gz
 tar -xzvf /etc/AdGuardHome/AdGuardHome_linux_armv7.tar.gz -C /etc/
 rm -rf /etc/AdGuardHome/AdGuardHome_linux_armv7.tar.gz
